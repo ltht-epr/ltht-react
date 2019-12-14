67dc91adc63a2e4f43fee6928437c8cc8d81229a
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-14 15:44:37 +0000

    add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { css } from 'emotion'
+
+const styles = css`
+  -webkit-appearance: none;
+  -webkit-font-smoothing: antialiased;
+  background-color: #00823b;
+  border: none;
+  box-shadow: 0 2px 0 #003618;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1em;
+  line-height: 1.25;
+  outline-offset: -1px;
+  outline: 1px solid transparent;
+  padding: 0.526315em 0.789473em 0.263157em;
+  position: relative;
+  text-decoration: none;
+`
+
+const Button: React.FC = () => <button css={css}></button>
+
+export default Button
