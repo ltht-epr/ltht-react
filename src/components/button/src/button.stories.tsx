161050d6f11e412be4c3819288dae5528bce66d9
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-14 16:32:23 +0000

    button src folder

--- src/components/button/src/button.stories.tsx
+++ src/components/button/src/button.stories.tsx
@@ -1,8 +1,8 @@
 import { storiesOf } from '@storybook/react'
 import { DefaultButton } from './button.fixtures'
 
-const stories = storiesOf('Form/Buttons', module)
-const examples = storiesOf('Form/Buttons/Examples', module)
+const stories = storiesOf('Elements|Form/Buttons', module)
+const examples = storiesOf('Elements|Form/Buttons/Examples', module)
 
 stories.add('Component Default', DefaultButton)
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from './components/button'
+export { default as Button } from './components/button/src'
