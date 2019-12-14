d9a70b3554adca0ed95570734e5016eb6089565e
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-14 16:29:54 +0000

    remove test story

--- src/stories/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import { Example } from '..'
-
-storiesOf('Example', module).add('Default', () => <Example />)
-storiesOf('Example', module).add('With child text', () => <Example>Example</Example>)
