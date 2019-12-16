7facdb4ca3f253027c1a71674db415179b0ddac3
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-16 14:37:56 +0000

    add docs to stories

--- src/components/list/list.stories.tsx
+++ src/components/list/list.stories.tsx
@@ -1,5 +1,9 @@
-import { storiesOf } from '@storybook/react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+
 import { DefaultList } from './list.fixtures'
+
+addDecorator(withInfo)
 
 const stories = storiesOf('Elements|List', module)
 const examples = storiesOf('Elements|List/Examples', module)
