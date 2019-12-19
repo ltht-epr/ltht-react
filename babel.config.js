6461294c811aebcfe947ae61630f74e1adda86b2
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-19 14:36:13 +0000

    move build to babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
-module.exports = {
-  presets: ['@babel/preset-typescript', '@babel/preset-env'],
+const esModules = process.env.BABEL_ENV === 'es'
+
+const presets = ['@babel/preset-env', '@babel/preset-typescript']
+
+const plugins = ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread', 'emotion']
+
+const ignore = ['.spec.ts', '.test.ts', '-test.ts', '/__tests__/']
+
+if (!esModules) {
+  presets[0] = [
+    '@babel/preset-env',
+    {
+      loose: true,
+      modules: 'commonjs',
+    },
+  ]
+  plugins.push('add-module-exports')
+} else {
+  presets[0] = [
+    '@babel/preset-env',
+    {
+      loose: true,
+      modules: false,
+    },
+  ]
+}
+
+module.exports = api => {
+  api.cache(() => process.env.NODE_ENV)
+  return {
+    presets,
+    plugins,
+    ignore,
+  }
 }
