2054b46597f1c086a9709a290e0bfd7b0de74743
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-20 12:58:08 +0000

    lerna

--- components/button/src/index.ts
+++ components/button/src/index.ts
@@ -2,7 +2,7 @@ import { PatientResource } from '@ltht-react/types'
 
 const Print = (input: PatientResource) => console.log('hello, ', input.firstName)
 
-const Log = (input: string) => console.log('string ', input)
+const Log = (input: string) => console.log('string test', input)
 
 export default Print
 
