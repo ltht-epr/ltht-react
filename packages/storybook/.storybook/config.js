4770f07049078cb5e557430e7b9c42010f3af705
Jonny Smith <jonny.smith@digitaljs.com>
2019-12-27 09:26:04 +0000

    add viewports

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,4 +1,5 @@
 import { configure, addParameters } from '@storybook/react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 const req = require.context('../src/', true, /.stories.tsx$/)
 
@@ -6,8 +7,13 @@ function loadStories() {
   req.keys().forEach(req)
 }
 
+const viewports = {
+  ...INITIAL_VIEWPORTS,
+}
+
 addParameters({
   backgrounds: [{ name: 'canvas-background', value: '#d8d8d8', default: true }],
+  viewport: { viewports },
 })
 
 configure(loadStories, module)
