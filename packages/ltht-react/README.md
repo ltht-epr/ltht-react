9bdeaed8411114a129780a6843b0dd13098931cf
Jonny Smith <jonny.smith@digitaljs.com>
2020-01-03 11:35:17 +0000

    ltht-react package

---
+++ packages/ltht-react/README.md
@@ -0,0 +1,17 @@
+# Flag Summary
+
+<!-- STORY -->
+
+### Import
+
+```js
+import { FlagSummary } from '@ltht-react/components'
+```
+
+### Usage
+
+Simple
+
+```jsx
+<FlagSummary flags={flags} />
+```
---
+++ packages/ltht-react/src/index.ts
@@ -0,0 +1,4 @@
+export { default as FlagSummary } from '@ltht-react/flag-summary'
+export { default as ExclamationIcon } from '@ltht-react/exclamation-icon'
+export * from '@ltht-react/styles'
+export * from '@ltht-react/types'
