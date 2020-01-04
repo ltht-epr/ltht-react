6bca37e74dce0c91cbf46ae95ef4b7b60d0b2421
Jonny Smith <jonny.smith@digitaljs.com>
2020-01-04 14:10:20 +0000

    docgen wip

---
+++ packages/ltht-docgen/bin/doc-component.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const { version } = require('../package.json')
+const doc = require('../lib')
+
+program.version(version).usage('<glob-to-components> <markdown-file>')
+
+program.parse(process.argv)
+
+const glob = program.args[0]
+const markdown = program.args[1]
+
+doc.default(glob, markdown)
---
+++ packages/ltht-docgen/src/markdown/generateMarkdown.ts
@@ -0,0 +1,5 @@
+const generateMarkdown = (name: string, folderName: string, reactApi: string) => {
+  console.log('generateMarkdown ', name, folderName, reactApi)
+}
+
+export default generateMarkdown
