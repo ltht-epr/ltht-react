ff0e960e3a27719b8fc096bb23d912f94e6c1c1b
Jonny Smith <jonny.smith@digitaljs.com>
2020-01-06 11:42:00 +0000

    remove docgen

--- packages/ltht-docgen/src/markdown/generateMarkdown.ts
+++
@@ -1,5 +0,0 @@
-const generateMarkdown = (name: string, folderName: string, reactApi: string) => {
-  console.log('generateMarkdown ', name, folderName, reactApi)
-}
-
-export default generateMarkdown
