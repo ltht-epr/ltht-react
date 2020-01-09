30ddd015e748501d726e4b3b30a85aa8973f8959
Jonny Smith <jonny.asmith@digitaljs.com>
2020-01-09 12:02:29 +0000

    fix testMatch for Windows

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   transform: {
     '^.+\\.ts?(x)?$': 'ts-jest',
   },
-  testMatch: ['<rootDir>/{components,packages}/*/src/**/?(*.)(spec|specs|test|tests).ts?(x)'],
+  testMatch: [
+    '<rootDir>/components/*/src/**/test?(s).ts?(x)',
+    '<rootDir>/components/*/src/**/*.test?(s).ts?(x)',
+    '<rootDir>/packages/*/src/**/test?(s).ts?(x)',
+    '<rootDir>/packages/*/src/**/*.test?(s).ts?(x)',
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/scripts/setupTests.ts'],
   snapshotSerializers: ['jest-emotion', 'enzyme-to-json/serializer'],
