0fad9cb6303401b08fefcafb21351b113d6aa4d2
Jonathan Dyson <jonathan.dyson@nhs.net>
2020-02-17 14:22:15 +0000

    Delete old detail components

--- components/detail/src/atoms/detail-list-item-description.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DetailListItemDescription: React.FC = ({ children }) => {
-  return <dd>{children}</dd>
-}
-
-export default DetailListItemDescription
--- components/detail/src/atoms/detail-list-item-tag.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DetailListItemTag: React.FC = ({ children }) => {
-  return <dt>{children}</dt>
-}
-
-export default DetailListItemTag
