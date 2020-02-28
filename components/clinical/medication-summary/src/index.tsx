9c9fb389f7441e13064db9bb154594185439c827
Joe Steele <joseph.steele@nhs.net>
2020-02-28 11:12:23 +0000

    Meds summary with dosage

--- components/clinical/medication-summary/src/index.tsx
+++ components/clinical/medication-summary/src/index.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
 import { MedicationList } from '@ltht-react/types'
+import { DescriptionList, DescriptionListTerm, DescriptionListDescription } from '@ltht-react/description-list'
+
+import { codeableConceptDisplaySummary as displaySummary } from '@ltht-react/utils'
 
 const MedicationSummary: React.FC<Props> = ({ medicationlist = undefined }) => {
   if (!medicationlist) {
     return <div>No Data</div>
   }
 
-  const meds = medicationlist.medicationStatements?.map(x => {
-    const coding = x?.medication.code?.coding
-    if (!coding) {
-      return null
+  const meds2 = new Array<JSX.Element>()
+
+  medicationlist.medicationStatements?.forEach(stmt => {
+    const code = stmt?.medication.code
+    if (!code) {
+      return
     }
 
-    return <div>{coding[0]?.display}</div>
+    meds2.push(<DescriptionListTerm>{displaySummary(code)}</DescriptionListTerm>)
+    meds2.push(<DescriptionListDescription>{stmt?.dosage?.map(x => x?.text).join(', ')}</DescriptionListDescription>)
   })
 
-  return <div>{meds}</div>
+  return <DescriptionList>{meds2}</DescriptionList>
 }
 
 interface Props {
