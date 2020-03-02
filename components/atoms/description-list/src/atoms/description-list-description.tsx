11a7d27dfb47bc46a1cfff8c7a39e2179fd4402e
Pritpal Saggu <pritpal.saggu@leedsth.nhs.uk>
2020-03-02 14:34:34 +0000

    Updated text styling for list term and description

--- components/atoms/description-list/src/atoms/description-list-description.tsx
+++ components/atoms/description-list/src/atoms/description-list-description.tsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
 import React from 'react'
 import { css, jsx } from '@emotion/core'
-import { TEXT_PRIMARY_COLOUR } from '@ltht-react/styles'
+import { TEXT_SECONDARY_COLOUR } from '@ltht-react/styles'
 
 const styles = css`
-  color: ${TEXT_PRIMARY_COLOUR};
+  color: ${TEXT_SECONDARY_COLOUR};
   margin-top: 0.25rem;
 `
 
--- components/atoms/description-list/src/atoms/description-list-term.tsx
+++ components/atoms/description-list/src/atoms/description-list-term.tsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
 import React from 'react'
 import { css, jsx } from '@emotion/core'
-import { TEXT_SECONDARY_COLOUR } from '@ltht-react/styles'
+import { TEXT_PRIMARY_COLOUR } from '@ltht-react/styles'
 
 const styles = css`
-  color: ${TEXT_SECONDARY_COLOUR};
+  color: ${TEXT_PRIMARY_COLOUR};
   margin-top: 0.5rem;
 `
 
