5012931c93b51f1a8a31e61cd3b5b11f2096efc4
Joe Steele <joseph.steele@nhs.net>
2020-03-13 08:46:32 +0000

    Guidance styling adjusted for card header styles

--- components/clinical/guidance-summary/src/index.tsx
+++ components/clinical/guidance-summary/src/index.tsx
@@ -2,36 +2,22 @@
 import React from 'react'
 import { css, jsx } from '@emotion/core'
 
-import { CARD_BORDER_COLOUR, TABLET_MEDIA_QUERY, DESKTOP_MEDIA_QUERY } from '@ltht-react/styles'
+import { CARD_BORDER_COLOUR } from '@ltht-react/styles'
 import { GuidanceList } from '@ltht-react/types'
 import GuidanceItem from './guidance-item'
 
 const styles = {
   root: css`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    flex: 1 0 51%;
-    margin-left: -1rem;
-    margin-top: -0.5rem;
-    & > div {
+    > div {
+      margin-right: -0.5rem;
+      padding-bottom: 0.5rem;
+      > ul {
+        padding: 0.25rem 0 0 1rem;
+      }
+    }
+    > div:not(:first-child) {
       border-top: 1px solid ${CARD_BORDER_COLOUR};
-      margin: 0.5rem 0 0 1rem;
       padding-top: 0.5rem;
-      & li {
-        margin-left: 1rem;
-      }
-    }
-    ${TABLET_MEDIA_QUERY} {
-      & > div {
-        flex: 1 0 34%;
-        max-width: 50%;
-      }
-    }
-    ${DESKTOP_MEDIA_QUERY} {
-      & > div {
-        flex: 1;
-      }
     }
   `,
 }
