6ca96067d00f6192bc3c88eaacb96304a9d11607
Jonathan Dyson <jonathan.dyson@nhs.net>
2020-04-03 16:58:23 +0100

    Flag Detail

--- packages/storybook/src/organisms/clinical/flags/flag-detail.stories.tsx
+++ packages/storybook/src/organisms/clinical/flags/flag-detail.stories.tsx
@@ -4,6 +4,8 @@ import JSXAddon from 'storybook-addon-jsx'
 
 import FlagDetail from '@ltht-react/flag-detail'
 import readme from '@ltht-react/flag-detail/README.md'
+import { Card, CardHeader, CardBody } from '@ltht-react/card'
+
 import flags from './flag.fixtures'
 
 const stories = storiesOf('Organisms - Clinical|Flag', module)
@@ -16,4 +18,13 @@ stories.addParameters({
   },
 })
 
-stories.addWithJSX('Detail', () => <FlagDetail flag={flags[0]} />)
+stories.addWithJSX('Detail', () => (
+  <Card noData={!flags}>
+    <CardHeader>
+      <h3>Flag</h3>
+    </CardHeader>
+    <CardBody>
+      <FlagDetail flag={flags[0]} />
+    </CardBody>
+  </Card>
+))
