181eddfb0f0625a84ec652f12bb9928a40891d94
Joe Steele <joseph.steele@nhs.net>
2020-04-07 08:28:51 +0100

    Documents click handler testing

--- packages/storybook/src/organisms/clinical/documents/document-summary.stories.tsx
+++ packages/storybook/src/organisms/clinical/documents/document-summary.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { useState } from '@storybook/addons'
 import JSXAddon from 'storybook-addon-jsx'
 
 import DocumentSummary from '@ltht-react/document-summary'
 import readme from '@ltht-react/document-summary/README.md'
 import { Card, CardHeader, CardBody } from '@ltht-react/card'
+import { DocumentReference } from '@ltht-react/types'
 import documents from './document-summary.fixture'
 
 const stories = storiesOf('Organisms - Clinical|Document', module)
@@ -18,14 +20,23 @@ stories.addParameters({
 })
 
 stories.addWithJSX('Summary', () => {
+  const [doc, setDoc] = useState<DocumentReference | undefined>(undefined)
+
+  const clickHandler = (document: DocumentReference): void => {
+    setDoc(document)
+  }
+
   return (
-    <Card noData={!documents}>
-      <CardHeader>
-        <h3>Documents</h3>
-      </CardHeader>
-      <CardBody>
-        <DocumentSummary documents={documents} />
-      </CardBody>
-    </Card>
+    <div>
+      <Card noData={!documents}>
+        <CardHeader>
+          <h3>Documents</h3>
+        </CardHeader>
+        <CardBody>
+          <DocumentSummary documents={documents} clickHandler={clickHandler} />
+        </CardBody>
+      </Card>
+      {doc && <div>{doc.id}</div>}
+    </div>
   )
 })
