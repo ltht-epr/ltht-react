d157549222e655a3e6d4e6c5e537e51275d471e2
Pritpal Saggu <pritpal.saggu@leedsth.nhs.uk>
2020-07-16 17:22:58 +0100

    Added more tests to Patient utility

--- packages/utils/src/atoms/patient.test.ts
+++ packages/utils/src/atoms/patient.test.ts
@@ -1,218 +1,269 @@
-import { Patient, Metadata, PatientIdentifierType, NhsNumberStatus } from '@ltht-react/types'
-import { formatNHSNumber, formatPatientName, nhsNumberStatus } from './patient'
+import {
+  Patient,
+  PatientIdentifierType,
+  NhsNumberStatus,
+  AddressUseCode,
+  PartialDateTimeKindCode,
+} from '@ltht-react/types'
+import { formatNHSNumber, formatPatientAddress, formatPatientAge, formatPatientName, nhsNumberStatus } from './patient'
 
-const metadata: Metadata = {
-  dataSources: [],
-  isRedacted: true,
-  requestedWhen: '',
+let patient: Patient
+
+const initializePatient = (): void => {
+  patient = {
+    id: 'test:patient',
+    metadata: {
+      dataSources: [],
+      isRedacted: true,
+      requestedWhen: '',
+    },
+    name: [],
+  }
 }
 
+beforeEach(() => {
+  initializePatient()
+})
+
 describe('formatNhsNumber', () => {
-  it('formats nhs number to empty string when patient does not have one', () => {
-    const patient: Patient = {
-      id: 'test:patient:1',
-      metadata,
-      name: [],
-    }
+  it('returns empty string when patient does not have one', () => {
     expect(formatNHSNumber(patient)).toEqual('')
   })
 
-  it('formats nhs number to empty when patient has an unofficial nhs number', () => {
-    const patient: Patient = {
-      id: 'test:patient:2',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: 'unofficial:system',
-          value: '9876-246',
-        },
-      ],
-    }
+  it('returns empty string when patient has an unofficial nhs number', () => {
+    patient.identifier = [
+      {
+        system: 'unofficial:system',
+        value: '9876-246',
+      },
+    ]
     expect(formatNHSNumber(patient)).toEqual('')
   })
 
-  it('formats nhs number correctly when patient has a official nhs number', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '9876543210',
-        },
-      ],
-    }
+  it('returns formatted nhs number when patient has a official nhs number', () => {
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+      },
+    ]
     expect(formatNHSNumber(patient)).toEqual('987 654 3210')
   })
 
-  it('formats nhs number correctly when patient has both official and unofficial nhs number', () => {
-    const patient: Patient = {
-      id: 'test:patient:4',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '9876543210',
-        },
-        {
-          system: 'unofficial:system',
-          value: '9876-246',
-        },
-      ],
-    }
+  it('returns formatted nhs number when patient has both official and unofficial nhs number', () => {
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+      },
+      {
+        system: 'unofficial:system',
+        value: '9876-246',
+      },
+    ]
     expect(formatNHSNumber(patient)).toEqual('987 654 3210')
   })
 
-  it('formats nhs number doesnot format when patient has a official nhs number with greater than 10 chars', () => {
-    const patient: Patient = {
-      id: 'test:patient:5',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '98765432109',
-        },
-      ],
-    }
+  it('returns non-formatted nhs number when patient has a official nhs number with greater than 10 chars', () => {
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '98765432109',
+      },
+    ]
     expect(formatNHSNumber(patient)).toEqual('98765432109')
   })
 
-  it('formats nhs number doesnot format when patient has a official nhs number with less than 10 chars', () => {
-    const patient: Patient = {
-      id: 'test:patient:6',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '98765432',
-        },
-      ],
-    }
+  it('returns non-formatted nhs number when patient has a official nhs number with less than 10 chars', () => {
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '98765432',
+      },
+    ]
     expect(formatNHSNumber(patient)).toEqual('98765432')
   })
 })
 
-describe('formatPatientName', () => {
-  it('returns empty string when name is not specified', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
+describe('formatPatientAddress', () => {
+  it('returns empty string when Address is not specified', () => {
+    patient.address = null
+    expect(formatPatientAddress(patient)).toEqual('')
+  })
+
+  it('returns empty string when Address collection is empty', () => {
+    patient.address = []
+    expect(formatPatientAddress(patient)).toEqual('')
+  })
+
+  it('returns empty string when no home address is present', () => {
+    patient.address = [{ use: AddressUseCode.Work, text: 'some work address' }]
+    expect(formatPatientAddress(patient)).toEqual('')
+  })
+
+  it('returns empty string when no active home address is present', () => {
+    patient.address = [{ use: AddressUseCode.Home, text: 'old home address', period: { end: { value: '2020-02-03' } } }]
+    expect(formatPatientAddress(patient)).toEqual('')
+  })
+
+  it('returns Address when active home address is present', () => {
+    patient.address = [{ use: AddressUseCode.Home, text: 'home address', period: {} }]
+    expect(formatPatientAddress(patient)).toEqual('home address')
+  })
+})
+
+describe('formatPatientAge', () => {
+  it('returns empty string when birth date is not specified', () => {
+    expect(formatPatientAge(patient)).toEqual('')
+  })
+
+  it('returns formatted age when birth date is specified (over 18)', () => {
+    let date = new Date()
+    let currentYear = date.getFullYear()
+    date.setFullYear(currentYear - 21)
+
+    patient.birthDate = {
+      kind: PartialDateTimeKindCode.Date,
+      value: date.toISOString(),
     }
+    expect(formatPatientAge(patient)).toEqual('21y')
+  })
+
+  it('returns formatted age when birth date is specified (under 18 and over 2)', () => {
+    let date = new Date()
+    let currentYear = date.getFullYear()
+    let currentMonth = date.getMonth()
+    date.setFullYear(currentYear - 15)
+    date.setMonth(0) // set to jan
+
+    patient.birthDate = {
+      kind: PartialDateTimeKindCode.Date,
+      value: date.toISOString(),
+    }
+    expect(formatPatientAge(patient)).toEqual(`15y ${currentMonth}m`)
+  })
+
+  it('returns formatted age when birth date is specified (under 2 and over 1)', () => {
+    let date = new Date()
+    let currentYear = date.getFullYear()
+    let currentDate = date.getDate()
+    date.setFullYear(currentYear - 1)
+    date.setDate(0) // set to last day of previous month
+
+    patient.birthDate = {
+      kind: PartialDateTimeKindCode.Date,
+      value: date.toISOString(),
+    }
+    expect(formatPatientAge(patient)).toEqual(`12m ${currentDate + 1}d`)
+  })
+})
+
+describe('formatPatientName', () => {
+  it('returns empty string when name collection is empty', () => {
     expect(formatPatientName(patient)).toEqual('')
   })
 })
 
 describe('nhsNumberStatus', () => {
   it('returns Unknown status when identifier is null', () => {
-    const patient: Patient = {
-      id: 'test:patient:1',
-      metadata,
-      name: [],
-    }
+    patient.identifier = null
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Unknown)
   })
 
   it('returns Unknown status when identifier is empty', () => {
-    const patient: Patient = {
-      id: 'test:patient:2',
-      metadata,
-      name: [],
-      identifier: [],
-    }
+    patient.identifier = []
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Unknown)
   })
 
   it('returns Unknown status when identifier system is not Nhs Number', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.PasNumber,
-          value: '0123456',
-        },
-      ],
-    }
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.PasNumber,
+        value: '0123456',
+      },
+    ]
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Unknown)
   })
 
   it('returns Unknown status when there is no verification code', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '9876543210',
-        },
-      ],
-    }
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+      },
+    ]
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Unknown)
   })
 
   it('returns Not Verified status when verification code is set to 02', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '9876543210',
-          extension: [
-            {
-              url: 'test',
-              valueString: 'test',
-              valueCodeableConcept: {
-                coding: [
-                  {
-                    code: '02',
-                    display: 'Number present and not verified',
-                  },
-                ],
-              },
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+        extension: [
+          {
+            url: 'test',
+            valueString: 'test',
+            valueCodeableConcept: {
+              coding: [
+                {
+                  code: '02',
+                  display: 'Number present and not verified',
+                },
+              ],
             },
-          ],
-        },
-      ],
-    }
+          },
+        ],
+      },
+    ]
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.NotVerified)
   })
 
   it('returns Verified status when verification code is set to 01', () => {
-    const patient: Patient = {
-      id: 'test:patient:3',
-      metadata,
-      name: [],
-      identifier: [
-        {
-          system: PatientIdentifierType.NhsNumber,
-          value: '9876543210',
-          extension: [
-            {
-              url: 'test',
-              valueString: 'test',
-              valueCodeableConcept: {
-                coding: [
-                  {
-                    code: '01',
-                    display: 'Number present and verified',
-                  },
-                ],
-              },
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+        extension: [
+          {
+            url: 'test',
+            valueString: 'test',
+            valueCodeableConcept: {
+              coding: [
+                {
+                  code: '01',
+                  display: 'Number present and verified',
+                },
+              ],
             },
-          ],
-        },
-      ],
-    }
+          },
+        ],
+      },
+    ]
     expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Verified)
+  })
+
+  it('returns Unknown status when verification code is set to values other that 01 or 02', () => {
+    patient.identifier = [
+      {
+        system: PatientIdentifierType.NhsNumber,
+        value: '9876543210',
+        extension: [
+          {
+            url: 'test',
+            valueString: 'test',
+            valueCodeableConcept: {
+              coding: [
+                {
+                  code: '03',
+                  display: 'Trace Required',
+                },
+              ],
+            },
+          },
+        ],
+      },
+    ]
+    expect(nhsNumberStatus(patient)).toEqual(NhsNumberStatus.Unknown)
   })
 })
