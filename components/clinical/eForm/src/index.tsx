691ea8cb4d64c0deee1098cc1cba898444da7591
Pritpal Saggu <pritpal.saggu@leedsth.nhs.uk>
2020-07-30 18:09:10 +0100

    Further updates for EForm component

--- components/clinical/eForm/src/index.tsx
+++ components/clinical/eForm/src/index.tsx
@@ -1,1 +1,42 @@
-export * from './molecules/eForm'
+/** @jsx jsx */
+import React from 'react'
+import { css, jsx, SerializedStyles } from '@emotion/core'
+
+import { CSS_RESET, EFORM_BACKGROUND_COLOUR } from '@ltht-react/styles'
+
+const styles = (): SerializedStyles => {
+  return css`
+    ${CSS_RESET}
+    background: ${EFORM_BACKGROUND_COLOUR};
+    padding: 0.5rem;
+    border-radius: 4px;
+    box-shadow: 0px 2px 1px -1px rgba(102, 102, 102, 0.1), 0px 1px 1px 0px rgba(102, 102, 102, 0.15),
+      0px 1px 3px 0px rgba(102, 102, 102, 0.6);
+    -webkit-font-smoothing: antialiased;
+
+    iframe {
+        width: 100%;
+        border: none;
+    }
+  `
+}
+
+const EForm: React.FC<Props> = ({ url, messageHandler }) => {
+  React.useEffect(() => {
+    window.addEventListener('message', messageHandler)
+    return (): void => window.removeEventListener('message', messageHandler)
+  })
+
+  return (
+    <div css={styles}>
+      <iframe src={url} title="eForm" />
+    </div>
+  )
+}
+
+interface Props {
+  url: string
+  messageHandler(event: unknown): void
+}
+
+export default EForm
---
+++ packages/storybook/src/organisms/clinical/eForm/eForm.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import JSXAddon from 'storybook-addon-jsx'
+
+import EForm from '@ltht-react/eForm'
+import readme from '@ltht-react/eForm/README.md'
+
+const stories = storiesOf('Organisms - Clinical|EForm', module)
+
+stories.addWithJSX = JSXAddon.addWithJSX
+
+stories.addParameters({
+  readme: {
+    sidebar: readme,
+  },
+})
+
+const handler = (event: unknown): void => {
+  // eslint-disable-next-line no-console
+  console.log(event)
+}
+
+stories.addWithJSX('NEWS', () => {
+  return <EForm url="https://www.wikipedia.org/" messageHandler={handler} />
+})
