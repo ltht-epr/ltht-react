4614de102da0e0a7a10081f4180a318c1565e529
Phil McVay <philmcvay@airelogic.com>
2021-02-11 16:29:50 +0000

    eslint-plugin upgrades

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'import/no-anonymous-default-export': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
