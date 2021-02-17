bf80edb56f8a0be99d097a9cfa644d2da88c6fbe
Phil McVay <philmcvay@airelogic.com>
2021-02-17 16:20:16 +0000

    Added data-testids to missing data summaries

--- components/clinical/shared/summary/src/organisms/missing-data-summary.tsx
+++ components/clinical/shared/summary/src/organisms/missing-data-summary.tsx
@@ -7,10 +7,10 @@ const MissingDataSummary: FC<Props> = ({ type, noData, partialData, completeData
   const missingDataSection =
     missingData != null && missingData.length > 0 ? (
       <>
-        <Card.Banner status="warning">
+        <Card.Banner data-testid="missing-data-count" status="warning">
           <CounterIcon size="large" status="amber" value={missingData.length} /> Missing Data
         </Card.Banner>
-        <Card.List>
+        <Card.List data-testid="missing-data-list">
           {missingData.map((data, index) => (
             <Card.ListItem key={`${type}-missing-data-${index}`}>{data}</Card.ListItem>
           ))}
@@ -23,10 +23,10 @@ const MissingDataSummary: FC<Props> = ({ type, noData, partialData, completeData
   const partialDataSection =
     partialData != null && partialData.length > 0 ? (
       <>
-        <Card.Banner status="warning">
+        <Card.Banner data-testid="partial-data-count" status="warning">
           <CounterIcon size="large" status="amber" value={partialData.length} /> Partial Data
         </Card.Banner>
-        <Card.List>
+        <Card.List data-testid="partial-data-list">
           {partialData.map((data, index) => (
             <Card.ListItem key={`${type}-partial-data-${index}`}>{data}</Card.ListItem>
           ))}
@@ -39,10 +39,10 @@ const MissingDataSummary: FC<Props> = ({ type, noData, partialData, completeData
   const completeDataSection =
     completeData != null && completeData.length > 0 ? (
       <>
-        <Card.Banner status="info">
+        <Card.Banner data-testid="complete-data-count" status="info">
           <CounterIcon size="large" status="info" value={completeData.length} /> Complete Data
         </Card.Banner>
-        <Card.List>
+        <Card.List data-testid="complete-data-list">
           {completeData.map((data, index) => (
             <Card.ListItem key={`${type}-complete-data-${index}`}>{data}</Card.ListItem>
           ))}
@@ -55,10 +55,10 @@ const MissingDataSummary: FC<Props> = ({ type, noData, partialData, completeData
   const noDataSection =
     noData != null && noData.length > 0 ? (
       <>
-        <Card.Banner status="info">
+        <Card.Banner data-testid="no-data-count" status="info">
           <CounterIcon size="large" status="info" value={noData.length} /> No Data
         </Card.Banner>
-        <Card.List>
+        <Card.List data-testid="no-data-list">
           {noData.map((data, index) => (
             <Card.ListItem key={`${type}-no-data-${index}`}>{data}</Card.ListItem>
           ))}
