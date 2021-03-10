c676fbe9be453b90698c6bdfa5ca6b734e059993
Phil McVay <philmcvay@airelogic.com>
2021-03-10 15:48:59 +0000

    Added Select component

--- components/styled/select/src/index.tsx
+++ components/styled/select/src/index.tsx
@@ -1,5 +1,98 @@
-import { FC } from 'react'
+import { FC, HTMLAttributes, useEffect, useRef, useState } from 'react'
+import styled from '@emotion/styled'
+import { CSS_RESET, DESKTOP_MEDIA_QUERY } from '@ltht-react/styles'
+import { ChevronIcon } from '@ltht-react/icon'
 
-const Select: FC = () => <div>Select</div>
+import Option, { IProps as OptionProps } from './atoms/option'
+
+const StyledSelect = styled.div`
+  ${CSS_RESET};
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`
+
+const SelectTrigger = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  min-height: 32px;
+  border: 1px solid black;
+  padding: 0.5rem 1rem;
+  background: white;
+  cursor: pointer;
+
+  ${DESKTOP_MEDIA_QUERY} {
+    min-height: 39px;
+  }
+`
+
+const ActiveOption = styled.span`
+  font-weight: bold;
+  flex: 1;
+`
+
+const SelectOptions = styled.div<ISelectOptions>`
+  width: 100%;
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  position: absolute;
+  top: 100%;
+  left: 0;
+`
+
+const Select: FC<IProps> & SelectComposition = ({ children, activeOption = '', ...rest }) => {
+  const [open, setOpen] = useState(false)
+  const selectRef = useRef<HTMLDivElement>(null)
+
+  const handleOnEscapeKey = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      setOpen(false)
+    }
+  }
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (selectRef.current && !selectRef.current.contains(e.target as Node)) {
+      setOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleOnEscapeKey, true)
+    document.addEventListener('click', handleOutsideClick, true)
+
+    // Cleanup
+    return () => {
+      document.removeEventListener('keydown', handleOnEscapeKey, true)
+      document.removeEventListener('click', handleOutsideClick, true)
+    }
+  })
+
+  return (
+    <StyledSelect {...rest} ref={selectRef}>
+      <SelectTrigger onClick={() => setOpen((open) => !open)}>
+        <ActiveOption>{activeOption}</ActiveOption>
+        <ChevronIcon direction={open ? 'up' : 'down'} size="small" />
+      </SelectTrigger>
+      <SelectOptions open={open} onClick={() => setOpen(false)}>
+        {children}
+      </SelectOptions>
+    </StyledSelect>
+  )
+}
+
+Select.Option = Option
+
+interface SelectComposition {
+  Option: FC<OptionProps>
+}
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+  activeOption?: string
+}
+
+interface ISelectOptions {
+  open: boolean
+}
 
 export default Select
---
+++ packages/storybook/src/ui/atoms/select/select.stories.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import { Story } from '@storybook/react'
+import Select from '@ltht-react/select'
+import { DESKTOP_MEDIA_QUERY } from '@ltht-react/styles'
+
+const StyledSelectContainer = styled.div`
+  width: 100%;
+
+  ${DESKTOP_MEDIA_QUERY} {
+    width: 30%;
+  }
+`
+
+export const DropdownSelect: Story = () => {
+  const [activeOption, setActiveOption] = useState('')
+
+  const handleOptionClick = (id: string, displayName: string) => {
+    // eslint-disable-next-line no-console
+    console.log(`${id} clicked!`)
+    setActiveOption(displayName)
+  }
+
+  const options = [
+    {
+      id: 'active',
+      displayName: 'Active',
+      onClick: () => handleOptionClick('active', 'Active'),
+    },
+    {
+      id: 'all',
+      displayName: 'All',
+      onClick: () => handleOptionClick('all', 'All'),
+    },
+    {
+      id: 'custom',
+      displayName: 'Custom',
+      onClick: () => handleOptionClick('custom', 'Custom'),
+    },
+  ]
+
+  return (
+    <StyledSelectContainer>
+      <Select activeOption={activeOption}>
+        {options.map(({ id, displayName, onClick }) => (
+          <Select.Option key={`select-option-${id}`} id={id} onClick={onClick} active={displayName === activeOption}>
+            {displayName}
+          </Select.Option>
+        ))}
+      </Select>
+    </StyledSelectContainer>
+  )
+}
+
+export default { title: 'UI/Atoms/Select' }
