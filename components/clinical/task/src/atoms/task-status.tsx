2de754d1515003e18784eb476e2c7e5e68324199
Wes Spellman <wes.spellman@airelogic.com>
2021-12-16 13:55:20 +0000

    changed status colour

--- components/clinical/task/src/atoms/task-status.tsx
+++ components/clinical/task/src/atoms/task-status.tsx
@@ -4,8 +4,19 @@ import { TEXT_COLOURS } from '@ltht-react/styles'
 import { titleCase } from '@ltht-react/utils'
 import { TaskStatusCode } from '@ltht-react/types'
 
+const generateStatusColour = (status: TaskStatusCode) => {
+  switch (status) {
+    case TaskStatusCode.Overdue:
+      return `red`
+    case TaskStatusCode.Due:
+      return `#ffa500`
+    default:
+      return `${TEXT_COLOURS.SECONDARY.VALUE}`
+  }
+}
+
 const StyledTaskStatus = styled.small<IProps>`
-  color: ${({ status }) => (status === TaskStatusCode.Overdue ? 'red' : `${TEXT_COLOURS.SECONDARY.VALUE}`)};
+  color: ${({ status }) => generateStatusColour(status)};
 `
 
 const TaskStatus: FC<IProps> = ({ status }) => {
