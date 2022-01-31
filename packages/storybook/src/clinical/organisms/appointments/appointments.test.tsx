addb187c03fc52cd1d5e7354f5733f8f3a71d826
Wes Spellman <wes.spellman@airelogic.com>
2022-01-31 17:01:17 +0000

    appointment and care plan tests

--- packages/storybook/src/clinical/organisms/appointments/appointments.test.tsx
+++ packages/storybook/src/clinical/organisms/appointments/appointments.test.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import AppointmentSummary from '@ltht-react/appointment-summary'
-import appointments from './appointments.fixtures'
+import encounters from './appointments.fixtures'
 
 describe('Appointments', () => {
-  it('Renders', () => {
-    render(<AppointmentSummary encounter={appointments[0]} />)
+  it('Renders the appointments widget correctly', () => {
+    render(<AppointmentSummary encounter={encounters[0]} />)
+    expect(screen.getByText('01-Apr-2020')).toBeInTheDocument()
+    expect(screen.getByText('See GP regarding sore throat')).toBeInTheDocument()
+    expect(screen.getByText('Beeston Village Surgery')).toBeInTheDocument()
+    expect(screen.getByText('Planned')).toBeInTheDocument()
+  })
+})
+
+describe('Appointments redacted', () => {
+  it('Renders the appointments widget correctly when redacted', () => {
+    render(<AppointmentSummary encounter={encounters[3]} />)
+    expect(screen.getByText('Insufficient privileges')).toBeInTheDocument()
   })
 })
