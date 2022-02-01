cdb444ef8d84938fbf795386ba8b438781f87c77
Wes Spellman <wes.spellman@airelogic.com>
2022-02-01 16:52:01 +0000

    diagnosis and community treatment orders tests

--- packages/storybook/src/clinical/organisms/community-treatment-orders/community-treatment-orders.test.tsx
+++ packages/storybook/src/clinical/organisms/community-treatment-orders/community-treatment-orders.test.tsx
@@ -1,10 +1,41 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import CommunityTreatmentOrderSummary from '@ltht-react/community-treatment-order-summary'
 import orders from './community-treatment-orders.fixtures'
+import CommunityTreatmentOrderDetail from '@ltht-react/community-treatment-order-detail'
 
-describe('Community Treatment Orders', () => {
-  it('renders without crashing', () => {
+describe('Community Treatment Orders details', () => {
+  it('renders', () => {
+    render(<CommunityTreatmentOrderDetail communityTreatmentOrder={orders[0]} />)
+    expect(screen.getByText('Consent')).toBeInTheDocument()
+    expect(screen.getByText('Consent Not Given')).toBeInTheDocument()
+
+    expect(screen.getByText('Legal Status')).toBeInTheDocument()
+    expect(screen.getByText('Approved')).toBeInTheDocument()
+
+    expect(screen.getByText('Period')).toBeInTheDocument()
+    expect(screen.getByText('01-Jan-2013 to 02-Feb-2014')).toBeInTheDocument()
+
+    expect(screen.getByText('Restrictions')).toBeInTheDocument()
+    expect(screen.getByText('No Restrictions')).toBeInTheDocument()
+  })
+})
+
+describe('Community Treatment Orders summary', () => {
+  it('renders', () => {
     render(<CommunityTreatmentOrderSummary communityTreatmentOrder={orders[0]} />)
+    expect(screen.getByText('Approved')).toBeInTheDocument()
+    expect(screen.getByText('No restrictions')).toBeInTheDocument()
+
+    expect(screen.getByText('01-Jan-2013 to 02-Feb-2014')).toBeInTheDocument()
+    expect(screen.getByText('Consent Not Given')).toBeInTheDocument()
+  })
+})
+
+describe('Community Treatment Orders redacted', () => {
+  it('renders', () => {
+    render(<CommunityTreatmentOrderSummary communityTreatmentOrder={orders[1]} />)
+    expect(screen.getByText('Insufficient privileges')).toBeInTheDocument()
+    expect(screen.getByText('02-Jan-2020 to Present')).toBeInTheDocument()
   })
 })
--- packages/storybook/src/clinical/organisms/diagnosis/diagnosis.test.tsx
+++ packages/storybook/src/clinical/organisms/diagnosis/diagnosis.test.tsx
@@ -1,10 +1,69 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import DiagnosisSummary from '@ltht-react/diagnosis-summary'
 import conditions from './diagnosis.fixtures'
+import DiagnosisDetail from '@ltht-react/diagnosis-detail'
 
-describe('Conditions', () => {
+describe('Summary', () => {
+  it('Renders', () => {
+    render(<DiagnosisSummary condition={conditions[1]} />)
+    expect(screen.getByText('Diabetes mellitus type 2')).toBeInTheDocument()
+    expect(screen.getByText('Problem - Mild')).toBeInTheDocument()
+
+    expect(screen.getByText('01-Jan-2020')).toBeInTheDocument()
+    expect(screen.getByText('Remission - Refuted')).toBeInTheDocument()
+  })
+})
+
+describe('Redacted', () => {
   it('Renders', () => {
     render(<DiagnosisSummary condition={conditions[0]} />)
+    expect(screen.getByText('Insufficient privileges')).toBeInTheDocument()
+    expect(screen.getByText('07-Aug-2018')).toBeInTheDocument()
+  })
+})
+
+describe('Detail', () => {
+  it('Renders', () => {
+    render(<DiagnosisDetail condition={conditions[0]} />)
+    expect(screen.getByText('Diagnosis')).toBeInTheDocument()
+    expect(screen.getByText('Heel Pain')).toBeInTheDocument()
+
+    expect(screen.getByText('Onset Date')).toBeInTheDocument()
+    expect(screen.getByText('Sep-2016')).toBeInTheDocument()
+
+    expect(screen.getByText('Clinical Status')).toBeInTheDocument()
+    expect(screen.getByText('Active')).toBeInTheDocument()
+
+    expect(screen.getByText('Verification Status')).toBeInTheDocument()
+    expect(screen.getByText('Confirmed')).toBeInTheDocument()
+
+    expect(screen.getByText('Category')).toBeInTheDocument()
+    expect(screen.getByText('Category A')).toBeInTheDocument()
+    expect(screen.getByText('Category B')).toBeInTheDocument()
+
+    expect(screen.getByText('Severity')).toBeInTheDocument()
+    expect(screen.getByText('Moderate')).toBeInTheDocument()
+
+    expect(screen.getByText('Location')).toBeInTheDocument()
+    expect(screen.getByText('Base of phalanx of foot')).toBeInTheDocument()
+    expect(screen.getByText('Subcutaneous tissue of lateral border of sole of foot')).toBeInTheDocument()
+
+    expect(screen.getByText('Evidence')).toBeInTheDocument()
+    expect(screen.getByText('Blister of foot without infection')).toBeInTheDocument()
+    expect(screen.getByText('Nonvenomous insect bite of foot with indection')).toBeInTheDocument()
+
+    expect(screen.getByText('Stage')).toBeInTheDocument()
+    expect(screen.getByText('Clinical stage I B')).toBeInTheDocument()
+
+    expect(screen.getByText('Asserted By')).toBeInTheDocument()
+    expect(screen.getByText('BROWN, David (Dr)')).toBeInTheDocument()
+
+    expect(screen.getByText('Asserted Date')).toBeInTheDocument()
+    expect(screen.getByText('07-Aug-2018')).toBeInTheDocument()
+
+    expect(screen.getByText('Data Source(s)')).toBeInTheDocument()
+    expect(screen.getByText('Mock Source A')).toBeInTheDocument()
+    expect(screen.getByText('Mock Source B')).toBeInTheDocument()
   })
 })
