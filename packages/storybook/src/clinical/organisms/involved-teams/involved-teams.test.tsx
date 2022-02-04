95ef6b03fe181d074289838413dfc1a9e57d1541
Wes Spellman <wes.spellman@airelogic.com>
2022-02-04 17:18:58 +0000

    involved-teams-tests

--- packages/storybook/src/clinical/organisms/involved-teams/involved-teams.test.tsx
+++ packages/storybook/src/clinical/organisms/involved-teams/involved-teams.test.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import InvolvedTeamSummary from '@ltht-react/involved-team-summary'
 import episodes from './involved-teams.fixtures'
 
-describe('Involved Teams', () => {
+describe('Involved Teams Summary', () => {
+  it('Renders', () => {
+    render(<InvolvedTeamSummary episodeOfCare={episodes[1]} />)
+    expect(screen.getByText('LYPFT Inbound Care Team')).toBeInTheDocument()
+    expect(screen.getByText('Joe Bloggs (0113 000000)')).toBeInTheDocument()
+
+    expect(screen.getByText('01-Dec-2019 to Present')).toBeInTheDocument()
+    expect(screen.getByText('Active')).toBeInTheDocument()
+  })
+})
+
+describe('Involved Teams Redacted', () => {
   it('Renders', () => {
     render(<InvolvedTeamSummary episodeOfCare={episodes[0]} />)
+    expect(screen.getByText('Insufficient privileges')).toBeInTheDocument()
+    expect(screen.getByText('01-Dec-2018 to 02-Jan-2020')).toBeInTheDocument()
   })
 })
