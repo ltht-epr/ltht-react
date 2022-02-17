ecfacbf8800217061473dfd322664515b960997d
George Busuioc <george.busuioc@airelogic.com>
2022-02-17 14:03:18 +0000

    Removing title as it wil lshow in detail banner

--- components/clinical/diagnosis-detail/src/index.tsx
+++ components/clinical/diagnosis-detail/src/index.tsx
@@ -33,12 +33,6 @@ const Seperator = styled.div`
   margin: 1rem 0;
 `
 
-const TitleStyle = styled.div`
-  font-size: 1rem;
-  font-weight: bold;
-  color: ${TEXT_COLOURS.SECONDARY.VALUE};
-`
-
 const DiagnosisDetail: FC<Props> = ({ condition, links }) => {
   const evidence = condition.evidence?.reduce((evidenceConcepts: Maybe<CodeableConcept>[], item) => {
     item?.code?.forEach((code) => evidenceConcepts.push(code))
@@ -49,7 +43,6 @@ const DiagnosisDetail: FC<Props> = ({ condition, links }) => {
     <>
       <TopSection>
         <CodeableConceptDetail term="Diagnosis" concept={condition.code} links={links} />
-        {condition?.extensionTemplateName && <TitleStyle>{condition?.extensionTemplateName}</TitleStyle>}
         <CodingListDetail term="Data Source(s)" codings={condition.metadata.dataSources} />
       </TopSection>
 
