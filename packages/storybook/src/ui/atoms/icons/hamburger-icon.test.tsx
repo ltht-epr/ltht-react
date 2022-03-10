d5bfab8dca16b86dad2779cba3b94157f131a732
Phil McVay <philmcvay@airelogic.com>
2022-03-10 16:09:49 +0000

    Added new icons

---
+++ packages/storybook/src/ui/atoms/icons/hamburger-icon.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+import { HamburgerIcon } from '@ltht-react/icon'
+
+describe('Comment Icon', () => {
+  it('Renders', () => {
+    render(<HamburgerIcon size="small" />)
+  })
+})
