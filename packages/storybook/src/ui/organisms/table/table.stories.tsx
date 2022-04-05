c7b5ffb08bec963fbe8baa2e6ba748140acfcfc4
Wes Spellman <wes.spellman@airelogic.com>
2022-04-05 10:44:46 +0100

    linting

--- packages/storybook/src/ui/organisms/table/table.stories.tsx
+++ packages/storybook/src/ui/organisms/table/table.stories.tsx
@@ -1,5 +1,6 @@
- import { Story } from '@storybook/react'
+import { Story } from '@storybook/react'
 import Table from '@ltht-react/table'
+
 export const table: Story = () => <Table />
+
 export default { title: 'Ui/Organisms/table' }
-  
