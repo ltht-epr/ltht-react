4ce027d582a0a091ae82021b13144b239938be61
Wes Spellman <wes.spellman@airelogic.com>
2022-04-11 14:39:55 +0100

    added stock data

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -1,27 +1,36 @@
 import { FC } from 'react'
-import MaterialTable, { MTableToolbar } from '@material-table/core'
+import MaterialTable, { Column } from '@material-table/core'
 
-const Table: FC<Props> = ({ records, definition }) => (
+interface IPerson {
+  firstName: string
+  lastName: string
+  birthYear: number
+  availability: boolean
+}
+
+const lookup = { true: 'Available', false: 'Unavailable' }
+
+const columns: Array<Column<IPerson>> = [
+  { title: 'First Name', field: 'firstName' },
+  { title: 'Last Name', field: 'lastName' },
+  { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+  { title: 'Availablity', field: 'availability', lookup },
+]
+
+const data: Array<IPerson> = [
+  { firstName: 'Tod', lastName: 'Miles', birthYear: 1987, availability: true },
+  { firstName: 'Jess', lastName: 'Smith', birthYear: 2000, availability: false },
+]
+
+const Table: FC<Props> = () => (
   <>
-    <MaterialTable
-      columns={definition}
-      data={records}
-      components={{
-        Toolbar: (props) => {
-          return (
-            <div style={{ backgroundColor: 'red' }}>
-              <MTableToolbar {...props} />
-            </div>
-          )
-        },
-      }}
-    />
+    <MaterialTable columns={columns} data={data} />
   </>
 )
 
 export default Table
 
 interface Props {
-  records: any
-  definition: any
+  // records: any
+  // definition: any
 }
--- packages/storybook/src/ui/organisms/table/table.stories.tsx
+++ packages/storybook/src/ui/organisms/table/table.stories.tsx
@@ -1,8 +1,8 @@
 import { Story } from '@storybook/react'
 import Table from '@ltht-react/table'
-import summaryDefinition from './table.fixtures.summaryDefinition'
-import summaryRecordsList from './table.fixtures.summaryRecords'
+// import summaryDefinition from './table.fixtures.summaryDefinition'
+// import summaryRecordsList from './table.fixtures.summaryRecords'
 
-export const table: Story = () => <Table records={summaryRecordsList} defintion={summaryDefinition} />
+export const table: Story = () => <Table />
 
 export default { title: 'Ui/Organisms/table' }
