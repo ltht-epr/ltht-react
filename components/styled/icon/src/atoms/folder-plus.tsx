d21f527efd75f69e59b256c1be39f429881a0d2f
Jonathan Dyson <jonathan.dyson@nhs.net>
2022-04-25 16:21:13 +0100

    Clickable folder icon and diagnosis changes

--- components/styled/icon/src/atoms/folder-plus.tsx
+++ components/styled/icon/src/atoms/folder-plus.tsx
@@ -1,14 +1,44 @@
-import { FC, HTMLAttributes } from 'react'
-import { calculateIconSize, IconSizes } from '@ltht-react/styles'
+import { FC, HTMLAttributes, MouseEvent } from 'react'
+import { calculateIconSize, IconSizes, LINK_COLOURS } from '@ltht-react/styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons'
+import styled from '@emotion/styled'
 
-const FolderPlusIcon: FC<FolderPlusIconProps> = ({ size, ...rest }) => (
-  <FontAwesomeIcon className="icon__folder__plus" icon={faFolderPlus} size={calculateIconSize(size)} {...rest} />
-)
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)<ControlProps>`
+  color: ${({ clickable }) => (clickable ? LINK_COLOURS.TEXT.DEFAULT : '')};
+
+  &:hover {
+    color: ${({ clickable }) => (clickable ? LINK_COLOURS.TEXT.HOVER : '')};
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : '')};
+  }
+`
+
+const FolderPlusIcon: FC<FolderPlusIconProps> = ({ size, clickHandler, ...rest }) => {
+  const handleClick = (e: MouseEvent<SVGSVGElement>): void => {
+    e.preventDefault()
+    e.stopPropagation()
+    clickHandler && clickHandler()
+  }
+
+  return (
+    <StyledFontAwesomeIcon
+      className="icon__folder__plus"
+      onClick={clickHandler && handleClick}
+      icon={faFolderPlus}
+      size={calculateIconSize(size)}
+      clickable={clickHandler !== undefined}
+      {...rest}
+    />
+  )
+}
+
+interface ControlProps {
+  clickable: boolean
+}
 
 interface FolderPlusIconProps extends HTMLAttributes<SVGElement> {
   size: IconSizes
+  clickHandler?(): void
 }
 
 export default FolderPlusIcon
