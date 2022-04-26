d6b2b7111659fe262f8dc19d4be938c4cdc860d5
Wes Spellman <wes.spellman@airelogic.com>
2022-04-26 13:48:39 +0100

    formatting

--- components/styled/icon/src/atoms/folder-plus.tsx
+++ components/styled/icon/src/atoms/folder-plus.tsx
@@ -3,11 +3,12 @@ import { calculateIconSize, IconSizes, LINK_COLOURS } from '@ltht-react/styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 import styled from '@emotion/styled'
+
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)<ControlProps>`
-  color: ${({ clickable }) => (clickable ? LINK_COLOURS.TEXT.DEFAULT : '')};
+  color: ${(clickable) => (clickable ? LINK_COLOURS.TEXT.DEFAULT : '')};
   &:hover {
-    color: ${({ clickable }) => (clickable ? LINK_COLOURS.TEXT.HOVER : '')};
-    cursor: ${({ clickable }) => (clickable ? 'pointer' : '')};
+    color: ${(clickable) => (clickable ? LINK_COLOURS.TEXT.HOVER : '')};
+    cursor: ${(clickable) => (clickable ? 'pointer' : '')};
   }
 `
 const FolderPlusIcon: FC<FolderPlusIconProps> = ({ size, ...rest }) => {
