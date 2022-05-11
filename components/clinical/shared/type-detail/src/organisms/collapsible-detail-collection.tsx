eee2636a74ed2cc8fea44a20df851e14ed49af9c
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-11 11:06:02 +0100

    Add hook to manage detail view state

--- components/clinical/shared/type-detail/src/organisms/collapsible-detail-collection.tsx
+++ components/clinical/shared/type-detail/src/organisms/collapsible-detail-collection.tsx
@@ -1,5 +1,5 @@
 import { DetailViewType, Maybe } from '@ltht-react/types'
-import { FC, Children, cloneElement } from 'react'
+import { FC, Children, cloneElement, useState, Dispatch, SetStateAction } from 'react'
 
 const CollapsibleDetailCollection: FC<CollapsibleDetailCollectionProps> = ({ children, viewType }) => {
   const showIfEmpty = viewType === DetailViewType.Expanded
@@ -12,6 +12,23 @@ export interface CollapsibleDetailCollectionProps {
   viewType?: Maybe<DetailViewType>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   children?: any
+}
+
+export const useDetailViewType = (): UseDetailViewType => {
+  const [viewType, setViewType] = useState(DetailViewType.Compact)
+
+  const toggleViewType = () => {
+    if (viewType === DetailViewType.Compact) setViewType(DetailViewType.Expanded)
+    else setViewType(DetailViewType.Compact)
+  }
+
+  return { viewType, toggle: toggleViewType, setViewType }
+}
+
+type UseDetailViewType = {
+  viewType: DetailViewType
+  toggle: () => void
+  setViewType: Dispatch<SetStateAction<DetailViewType>>
 }
 
 export default CollapsibleDetailCollection
