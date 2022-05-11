3acba0bb6bbc3aa52bef6aa511722eeed34a9ffb
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-11 13:48:37 +0100

    compact encounter-status-history-detail

--- components/clinical/hospital-stay-detail/src/molecules/encounter-status-history-detail.tsx
+++ components/clinical/hospital-stay-detail/src/molecules/encounter-status-history-detail.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import styled from '@emotion/styled'
-import { EncounterStatusHistory, Maybe } from '@ltht-react/types'
+import { EncounterStatusHistory, Maybe, Scalars } from '@ltht-react/types'
 import DescriptionList from '@ltht-react/description-list'
 import { NestedListDetail } from '@ltht-react/type-detail'
 import { periodSummaryText, titleCase } from '@ltht-react/utils'
@@ -14,29 +14,39 @@ const StyledListItem = styled.li`
   list-style: initial;
 `
 
-const EncounterStatusHistoryDetail: FC<Props> = ({ hospitalStatusHistories }) => (
-  <NestedListDetail term="Status History">
-    {hospitalStatusHistories?.map((item) => {
-      if (item?.status) {
-        return (
-          <StyledNestedList key={item.status}>
-            <StyledListItem>
-              {titleCase(item?.status)} - {periodSummaryText(item?.period)}
-            </StyledListItem>
-          </StyledNestedList>
-        )
-      }
-      return (
-        <StyledNestedList>
-          <DescriptionList.Description>{periodSummaryText(item?.period)}</DescriptionList.Description>
-        </StyledNestedList>
-      )
-    })}
-  </NestedListDetail>
-)
+const term = 'Status History'
+
+const EncounterStatusHistoryDetail: FC<Props> = ({ hospitalStatusHistories, showIfEmpty = true }) => {
+  if (hospitalStatusHistories && hospitalStatusHistories.length > 0)
+    return (
+      <NestedListDetail term={term}>
+        {hospitalStatusHistories?.map((item) => {
+          if (item?.status) {
+            return (
+              <StyledNestedList key={item.status}>
+                <StyledListItem>
+                  {titleCase(item?.status)} - {periodSummaryText(item?.period)}
+                </StyledListItem>
+              </StyledNestedList>
+            )
+          }
+          return (
+            <StyledNestedList>
+              <DescriptionList.Description>{periodSummaryText(item?.period)}</DescriptionList.Description>
+            </StyledNestedList>
+          )
+        })}
+      </NestedListDetail>
+    )
+  if (showIfEmpty === true) {
+    return <NestedListDetail term={term} />
+  }
+  return <></>
+}
 
 interface Props {
   hospitalStatusHistories?: Maybe<Array<Maybe<EncounterStatusHistory>>>
+  showIfEmpty?: Maybe<Scalars['Boolean']>
 }
 
 export default EncounterStatusHistoryDetail
