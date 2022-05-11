b735f342f9c79c5b0e18c403a541eb1cafd7ca9c
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-11 13:59:33 +0100

    Render tests for detail views

--- packages/storybook/src/clinical/organisms/community-treatment-orders/community-treatment-orders.test.tsx
+++ packages/storybook/src/clinical/organisms/community-treatment-orders/community-treatment-orders.test.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react'
 
 import CommunityTreatmentOrderSummary from '@ltht-react/community-treatment-order-summary'
+import CommunityTreatmentOrderDetail from '@ltht-react/community-treatment-order-detail'
 import orders from './community-treatment-orders.fixtures'
 
 describe('Community Treatment Orders', () => {
-  it('renders without crashing', () => {
-    render(<CommunityTreatmentOrderSummary communityTreatmentOrder={orders[0]} />)
+  describe('Summary', () => {
+    it('Renders', () => {
+      render(<CommunityTreatmentOrderSummary communityTreatmentOrder={orders[0]} />)
+    })
+  })
+  describe('Detail', () => {
+    it('Renders', () => {
+      render(<CommunityTreatmentOrderDetail communityTreatmentOrder={orders[0]} />)
+    })
   })
 })
--- packages/storybook/src/clinical/organisms/diagnosis/diagnosis.test.tsx
+++ packages/storybook/src/clinical/organisms/diagnosis/diagnosis.test.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react'
 
 import DiagnosisSummary from '@ltht-react/diagnosis-summary'
+import DiagnosisDetail from '@ltht-react/diagnosis-detail'
 import conditions from './diagnosis.fixtures'
 
 describe('Conditions', () => {
-  it('Renders', () => {
-    render(<DiagnosisSummary condition={conditions[0]} />)
+  describe('Summary', () => {
+    it('Renders', () => {
+      render(<DiagnosisSummary condition={conditions[0]} />)
+    })
+  })
+  describe('Detail', () => {
+    it('Renders', () => {
+      render(<DiagnosisDetail condition={conditions[0]} />)
+    })
   })
 })
