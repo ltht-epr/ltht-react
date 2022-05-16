e0f50842a802e7d7adf7a6938871cb0a2588e9f4
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-16 13:35:33 +0100

    Revert diagnosis compact view change

--- components/clinical/diagnosis-detail/src/index.tsx
+++ components/clinical/diagnosis-detail/src/index.tsx
@@ -45,7 +45,7 @@ const DiagnosisDetail: FC<Props> = ({ condition, links, viewType = DetailViewTyp
       condition?.extensionData.map((item, index) => (
         <>
           {index === 0 && <Seperator />}
-          <Questionnaire questionnaire={item} showTitle viewType={DetailViewType.Compact} />
+          <Questionnaire questionnaire={item} showTitle viewType={viewType} />
           <Seperator />
         </>
       ))}
