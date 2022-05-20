ecc7a9805539352c94b7dfa4dc621c51b93f8ac7
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-20 15:50:23 +0100

    Allergies detail add details and separator

--- components/clinical/allergy-detail/src/index.tsx
+++ components/clinical/allergy-detail/src/index.tsx
@@ -11,6 +11,7 @@ import {
   ResourceReferenceDetail,
   StringDetail,
 } from '@ltht-react/type-detail'
+import { TEXT_COLOURS } from '@ltht-react/styles'
 import AllergyOnset from './atoms/allergy-onset-detail'
 
 const TopSection = styled.div`
@@ -25,12 +26,25 @@ const TopSection = styled.div`
   }
 `
 
+const Seperator = styled.div`
+  height: 1px;
+  background: rgba(0, 0, 0, 0.125);
+  width: calc(100% + 12px);
+  margin: 1rem 0;
+`
+const Title = styled.h4`
+  margin: 1rem 0 1rem;
+  color: ${TEXT_COLOURS.SECONDARY.VALUE};
+`
+
 const AllergyDetail: FC<Props> = ({ allergy, showDates = true, viewType = DetailViewType.Compact }) => (
   <>
     <TopSection>
       <StringDetail term="Type" description={allergy.type?.toString()} />
       <CodingListDetail term="Data Source(s)" codings={allergy.metadata.dataSources} />
     </TopSection>
+    <Seperator />
+    <Title>Details</Title>
     <CollapsibleDetailCollection viewType={viewType}>
       <StringDetail term="Category" description={allergy?.category?.toString()} />
       <CodeableConceptDetail term="Allergy" concept={allergy.code} />
