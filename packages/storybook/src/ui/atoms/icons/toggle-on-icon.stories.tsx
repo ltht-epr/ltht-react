a9dba47278c9eb9bca09efb4ecb9a0a983a14499
Jack Thorpe <jack.thorpe@airelogic.com>
2022-05-24 12:00:28 +0100

    Add icons to storybook (missed)

--- packages/storybook/src/ui/atoms/icons/toggle-on-icon.stories.tsx
+++ packages/storybook/src/ui/atoms/icons/toggle-on-icon.stories.tsx
@@ -1,9 +1,13 @@
-import { render } from '@testing-library/react'
-
+import { Story } from '@storybook/react'
 import ToggleOnIcon from '@ltht-react/icon/src/atoms/toggle-on-icon'
 
-describe('Toggle On Icon', () => {
-  it('Renders', () => {
-    render(<ToggleOnIcon size="small" />)
-  })
-})
+// eslint-disable-next-line import/prefer-default-export
+export const ToggleOn: Story = () => (
+  <>
+    <ToggleOnIcon size="small" />
+    <br />
+    <ToggleOnIcon size="medium" />
+    <br />
+    <ToggleOnIcon size="large" />
+  </>
+)
