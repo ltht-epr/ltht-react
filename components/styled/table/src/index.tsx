51603bd8eca1263b43303ded504396fea40f0f55
Jonny Dyson <jsed500@googlemail.com>
2022-07-11 11:20:17 +0100

    Material UI Core and Interfaces

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -1,90 +1,85 @@
-// import React, { FC } from 'react'
-// import { Column, useTable } from 'react-table'
+import React, { FC } from 'react'
+import { Column, useTable } from 'react-table'
 
-import { FC } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import MaUTable from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 
-const Table: FC<IProps> = () => (
-  //  const data = React.useMemo(
-  //    () => [
-  //      {
-  //        col1: 'Hello',
-  //        col2: 'World',
-  //      },
-  //      {
-  //        col1: 'react-table',
-  //        col2: 'rocks',
-  //      },
-  //      {
-  //        col1: 'whatever',
-  //        col2: 'you want',
-  //      },
-  //    ],
-  //    []
-  //  )
+const Table: FC<IProps> = () => {
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  )
 
-  //  const columns = React.useMemo<Column>(
-  //    () => new Column[], null)
+  const columns: Column<ExampleObject>[] = [
+    {
+      Header: 'column 12345',
+      accessor: 'col1',
+    },
+    {
+      Header: () => 'column 2',
+      accessor: 'col2',
+    },
+  ]
 
-  //  const {
-  //    getTableProps,
-  //    getTableBodyProps,
-  //    headerGroups,
-  //    rows,
-  //    prepareRow,
-  //  } = useTable({ columns, data })
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<ExampleObject>({
+    columns,
+    data,
+  })
 
-  <></>
-)
-
-//  return (
-//   <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-//     <thead>
-//       {headerGroups.map(headerGroup => (
-//         <tr {...headerGroup.getHeaderGroupProps()}>
-//           {headerGroup.headers.map(column => (
-//             <th
-//               {...column.getHeaderProps()}
-//               style={{
-//                 borderBottom: 'solid 3px red',
-//                 background: 'aliceblue',
-//                 color: 'black',
-//                 fontWeight: 'bold',
-//               }}
-//             >
-//               {column.render('Header')}
-//             </th>
-//           ))}
-//         </tr>
-//       ))}
-//     </thead>
-//     <tbody {...getTableBodyProps()}>
-//       {rows.map(row => {
-//         prepareRow(row)
-//         return (
-//           <tr {...row.getRowProps()}>
-//             {row.cells.map(cell => {
-//               return (
-//                 <td
-//                   {...cell.getCellProps()}
-//                   style={{
-//                     padding: '10px',
-//                     border: 'solid 1px gray',
-//                     background: 'papayawhip',
-//                   }}
-//                 >
-//                   {cell.render('Cell')}
-//                 </td>
-//               )
-//             })}
-//           </tr>
-//         )
-//       })}
-//     </tbody>
-//   </table>
-// )
+  return (
+    <div>
+      <CssBaseline />
+      <MaUTable {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>
+                ))}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </MaUTable>
+    </div>
+  )
+}
 
 interface IProps {
   options: string
+}
+
+interface ExampleObject {
+  col1: string
+  col2: string
 }
 
 export default Table
