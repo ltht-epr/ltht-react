4cd718d646dafb218de5c587ade0cd00701018ac
Jack Thorpe <jack.thorpe@airelogic.com>
2022-08-02 08:38:26 +0100

    Add bullseye

---
+++ packages/storybook/src/ui/atoms/icons/bullseye-icon.test.tsx
@@ -0,0 +1,8 @@
+import { BullseyeIcon } from '@ltht-react/icon'
+import { render } from '@testing-library/react'
+
+describe('Bullseye Icon', () => {
+  it('Renders', () => {
+    render(<BullseyeIcon size="small" />)
+  })
+})
