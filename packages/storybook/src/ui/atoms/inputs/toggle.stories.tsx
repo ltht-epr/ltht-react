417c07b415a0bd193233f18ff348f1418e06a92f
Jack Thorpe <jack.thorpe@airelogic.com>
2022-08-04 16:08:40 +0100

    Add some styling o the story

--- packages/storybook/src/ui/atoms/inputs/toggle.stories.tsx
+++ packages/storybook/src/ui/atoms/inputs/toggle.stories.tsx
@@ -2,19 +2,35 @@
 import { Story } from '@storybook/react'
 import { Toggle } from '@ltht-react/input'
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import Card from '@ltht-react/card'
+import { Container } from '../../organisms/dashboard/dashboard'
+
+const StyledLabel = styled.label``
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 // eslint-disable-next-line import/prefer-default-export
 export const ToggleInput: Story = () => {
   const [isToggledOn, setIsToggledOn] = useState(false)
 
   return (
-    <>
-      <Toggle
-        onChange={(e) => setIsToggledOn((e.target as HTMLInputElement).checked)}
-        checked={isToggledOn}
-        id="toggle-1"
-      />
-      <label htmlFor="toggle-1"> Click me</label>
-    </>
+    <Card>
+      <Card.Header>
+        <Container>
+          <StyledWrapper>
+            <Toggle
+              onChange={(e) => setIsToggledOn((e.target as HTMLInputElement).checked)}
+              checked={isToggledOn}
+              id="toggle-1"
+            />
+            <StyledLabel htmlFor="toggle-1">Click me</StyledLabel>
+          </StyledWrapper>
+        </Container>
+      </Card.Header>
+    </Card>
   )
 }
