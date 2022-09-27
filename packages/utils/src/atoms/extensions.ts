b1f0131146bb71dd70c4087ca250c1698880f01f
George-Gabriel Busuioc <ggb.busuioc@gmail.com>
2022-09-27 13:13:24 +0100

    === instead of == in check

--- packages/utils/src/atoms/extensions.ts
+++ packages/utils/src/atoms/extensions.ts
@@ -3,7 +3,7 @@ import { Extension, Maybe } from '@ltht-react/types'
 const getBooleanExtension = (extensions: Maybe<Array<Maybe<Extension>>>, uri: string): Maybe<boolean> => {
   const booleanExtensionValue = extensions?.find((extension) => extension?.url === uri)?.valueBoolean
 
-  if (booleanExtensionValue == undefined) return null
+  if (booleanExtensionValue === undefined) return null
 
   return booleanExtensionValue
 }
