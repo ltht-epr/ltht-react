da0359e9973280808ce1d5f64d3172ec2e42930a
Sam Biram <samuel.biram@airelogic.com>
2022-10-12 19:50:04 +0100

    Create table mock data and storybook story to visualise it

--- packages/storybook/src/ui/organisms/table/table.stories.tsx
+++ packages/storybook/src/ui/organisms/table/table.stories.tsx
@@ -1,10 +1,14 @@
 import { Story } from '@storybook/react'
 import Table from '@ltht-react/table'
 import { summaryDefinition, summaryRecordsList } from './table.fixtures'
+import { mockSummaryDefinition, mockSummaryRecordsList } from './table.mockdata'
 
 export const VerticalTable: Story = () => <Table definition={summaryDefinition} records={summaryRecordsList} />
 export const HorizontalTable: Story = () => (
   <Table definition={summaryDefinition} records={summaryRecordsList} orientation="HORIZONTAL" />
+)
+export const MockDataVisualisation: Story = () => (
+  <Table definition={mockSummaryDefinition} records={mockSummaryRecordsList}></Table>
 )
 
 export default { title: 'UI/Organisms/Table' }
