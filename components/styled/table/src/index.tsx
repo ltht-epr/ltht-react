ddf5c851ecc2c5f8cb43fca3883b45fec240f84c
Sam Biram <samuel.biram@airelogic.com>
2022-10-12 11:04:06 +0100

    Check for empty array in addition to undefined

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -5,7 +5,7 @@ import HorizontalTable from './atoms/horizontal'
 import VerticalTable from './atoms/vertical'
 
 const Table: FC<IProps> = ({ definition, records, orientation = 'vertical' }) => {
-  if (!definition.item) {
+  if (!definition.item || definition.item.length === 0) {
     return <div>{`Could not render table. Definition items array was empty.`}</div>
   }
 
--- packages/storybook/src/ui/organisms/table/table.test.tsx
+++ packages/storybook/src/ui/organisms/table/table.test.tsx
@@ -5,12 +5,30 @@ import { mockSummaryDefinition, mockSummaryRecordsList } from './table.mockdata'
 describe('Table', () => {
   it('Renders', () => {
     render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} />)
+
+    expect(screen.getByRole('table')).toBeVisible()
   })
 
-  it('Presents warning text if insufficient data is provided', () => {
+  it('Presents warning text if definition item array is undefined', () => {
     const summaryDefinitionWithoutItems = { ...mockSummaryDefinition, item: undefined }
 
     render(<Table definition={summaryDefinitionWithoutItems} records={mockSummaryRecordsList} />)
+
+    expect(screen.getByText('Could not render table. Definition items array was empty.')).toBeVisible()
+  })
+
+  it('Presents warning text if definition item array is empty', () => {
+    const summaryDefinitionWithoutItems = { ...mockSummaryDefinition, item: [] }
+
+    render(<Table definition={summaryDefinitionWithoutItems} records={mockSummaryRecordsList} />)
+
+    expect(screen.getByText('Could not render table. Definition items array was empty.')).toBeVisible()
+  })
+
+  it('Presents warning text if insufficient data is provided', () => {
+    render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} />)
+
+    screen.debug()
 
     expect(screen.getByText('Could not render table. Definition items array was empty.')).toBeVisible()
   })
