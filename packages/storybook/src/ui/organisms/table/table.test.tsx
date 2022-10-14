974971e7cb76b3f584aa193d53299e1bd642a2df
Muhammad Saqib <muhammad.saqib@airelogic.com>
2022-10-14 13:50:46 +0100

    added tests for table

--- packages/storybook/src/ui/organisms/table/table.test.tsx
+++ packages/storybook/src/ui/organisms/table/table.test.tsx
@@ -29,4 +29,29 @@ describe('Table', () => {
   it('Renders Horizontally', () => {
     render(<Table definition={summaryDefinition} records={summaryRecordsList} orientation="HORIZONTAL" />)
   })
+
+  it('Renders horizontal table with multiple row headers. total 13 columns to be visible', () => {
+    render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} orientation="HORIZONTAL" />)
+
+    expect(screen.getAllByRole('columnheader').length).toBe(13)
+  })
+
+  it('Renders horizontal table with two data rows', () => {
+    render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} orientation="HORIZONTAL" />)
+
+    expect(screen.getAllByRole('rowgroup')[0].children.length).toBe(2)
+  })
+
+  it('Renders Vertically', () => {
+    render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} />)
+
+    expect(screen.getByRole('table')).toBeVisible()
+  })
+
+  it('Renders Vertical table with 5 rows in tbody', () => {
+    render(<Table definition={mockSummaryDefinition} records={mockSummaryRecordsList} />)
+
+    expect(screen.getByRole('table').children[1].tagName).toBe('TBODY')
+    expect(screen.getByRole('table').children[1].children.length).toBe(5)
+  })
 })
