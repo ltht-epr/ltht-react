084e68abc3ec2a1258a0a98e2804611cec2fce94
Sam Biram <samuel.biram@airelogic.com>
2022-10-18 10:15:13 +0100

    Implement sorting and state safety in Table

--- components/styled/table/src/atoms/table.tsx
+++ components/styled/table/src/atoms/table.tsx
@@ -1,66 +1,42 @@
-import { useTable, Column } from 'react-table'
+import { useTable, Column, useSortBy } from 'react-table'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import MaUTable from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 import styled from '@emotion/styled'
-import {
-  CSS_RESET,
-  TRANSLUCENT_BRIGHT_BLUE_TABLE,
-  TRANSLUCENT_GREY_TABLE,
-  TRANSLUCENT_MID_GREY,
-} from '@ltht-react/styles'
+import { TRANSLUCENT_BRIGHT_BLUE_TABLE, TRANSLUCENT_GREY_TABLE } from '@ltht-react/styles'
+import { useEffect, useState } from 'react'
 
 const Container = styled.div`
-  ${CSS_RESET};
   background-color: white;
-  border-radius: 12px;
-`
-
-const StyledTable = styled.table`
-  width: 100%;
-  border-radius: 12px;
-  border-collapse: collapse;
-  border: thin solid ${TRANSLUCENT_MID_GREY};
 `
 
 const StyledTableHeader = styled.th`
-  font-weight: bold
-  border-radius: 12px;
+  border: 1px solid rgba(200, 200, 200, 1);
 `
 
-const generateColumnsFromHeadersRecursively = (headers?: Header[]): Column<Record<string, string>>[] => {
-  if (!headers || headers.length < 1) {
-    return []
-  }
-
-  return headers.map((header) => {
-    if (header.subheaders) {
-      return {
-        Header: header.header,
-        accessor: header.accessor,
-        columns: generateColumnsFromHeadersRecursively(header.subheaders),
-      }
-    }
-
+const generateColumnsFromHeadersRecursively = (headers?: Header[]): Column<Record<string, string>>[] =>
+  (headers ?? []).map((header) => {
     return {
       Header: header.header,
       accessor: header.accessor,
+      columns: !!header.subheaders ? generateColumnsFromHeadersRecursively(header.subheaders) : undefined,
+      sortType: 'basic',
     }
   })
-}
 
-const generateRowsFromCellRows = (cellRows: CellRow[]): Record<string, string>[] => {
-  const mappedCells: Record<string, string>[] = []
-
-  cellRows.forEach((cellRow) => {
+const generateRowsFromCellRows = (cellRows: CellRow[]): Record<string, string>[] =>
+  cellRows.map((cellRow) => {
     const mappedCell: Record<string, string> = {}
 
     cellRow.cells.forEach((cell) => {
       mappedCell[cell.key] = cell.value
     })
 
-    mappedCells.push(mappedCell)
+    return mappedCell
   })
-
-  return mappedCells
-}
 
 export default function Table<TColumn, TRow>({
   tableData,
@@ -68,35 +44,51 @@ export default function Table<TColumn, TRow>({
   rowData,
   mapToTableData,
 }: IProps<TColumn, TRow>) {
-  const mappedTabledata =
-    columnData && rowData && mapToTableData
-      ? mapToTableData(columnData, rowData)
-      : tableData ?? { headers: [], rows: [] }
+  const [columns, setColumns] = useState<Column<Record<string, string>>[]>([])
+  const [data, setData] = useState<Record<string, string>[]>([])
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-    columns: generateColumnsFromHeadersRecursively(mappedTabledata.headers),
-    data: generateRowsFromCellRows(mappedTabledata.rows),
-  })
+  useEffect(() => {
+    const mappedTableData =
+      columnData && rowData && mapToTableData
+        ? mapToTableData(columnData, rowData)
+        : tableData ?? { headers: [], rows: [] }
+
+    setColumns(generateColumnsFromHeadersRecursively(mappedTableData.headers))
+    setData(generateRowsFromCellRows(mappedTableData.rows))
+  }, [tableData, columnData, rowData, mapToTableData])
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
 
   return (
     <Container>
-      <StyledTable {...getTableProps()}>
-        <thead>
+      <CssBaseline />
+      <MaUTable {...getTableProps()}>
+        <TableHead>
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <StyledTableHeader {...column.getHeaderProps()}>{column.render('Header')}</StyledTableHeader>
+                <StyledTableHeader {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
+                </StyledTableHeader>
               ))}
             </tr>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
           {rows.map((row) => {
             prepareRow(row)
             return (
-              <tr {...row.getRowProps()}>
+              <TableRow {...row.getRowProps()}>
                 {row.cells.map((cell, cellIdx) => (
-                  <td
+                  <TableCell
                     style={{
                       background: cellIdx % 2 === 1 ? TRANSLUCENT_GREY_TABLE : TRANSLUCENT_BRIGHT_BLUE_TABLE,
                       textAlign: 'center',
@@ -104,13 +96,13 @@ export default function Table<TColumn, TRow>({
                     {...cell.getCellProps()}
                   >
                     {cell.render('Cell')}
-                  </td>
+                  </TableCell>
                 ))}
-              </tr>
+              </TableRow>
             )
           })}
-        </tbody>
-      </StyledTable>
+        </TableBody>
+      </MaUTable>
     </Container>
   )
 }
