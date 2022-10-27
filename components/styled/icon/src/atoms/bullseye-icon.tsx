ffd25dcb67b4f52826f931b51138b9bcd97cf8a4
Sam Biram <samuel.biram@airelogic.com>
2022-10-27 15:46:53 +0100

    Make bullseye icon extendable

--- components/styled/icon/src/atoms/bullseye-icon.tsx
+++ components/styled/icon/src/atoms/bullseye-icon.tsx
@@ -1,13 +1,15 @@
-import { FC } from 'react'
+import { FC, HTMLAttributes } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBullseye } from '@fortawesome/free-solid-svg-icons'
 import { calculateIconSize, IconSizes } from '@ltht-react/styles'
 
 const BullseyeIcon: FC<BullseyeIconProps> = ({ size, ...rest }) => (
-  <FontAwesomeIcon className="icon__bullseye" icon={faBullseye} size={calculateIconSize(size)} {...rest} />
+  <div {...rest}>
+    <FontAwesomeIcon className="icon__bullseye" icon={faBullseye} size={calculateIconSize(size)} />
+  </div>
 )
 
-interface BullseyeIconProps {
+interface BullseyeIconProps extends HTMLAttributes<HTMLDivElement> {
   size: IconSizes
 }
 
--- packages/storybook/src/ui/atoms/icons/bullseye-icon.test.tsx
+++ packages/storybook/src/ui/atoms/icons/bullseye-icon.test.tsx
@@ -1,8 +1,31 @@
+import styled from '@emotion/styled'
 import { BullseyeIcon } from '@ltht-react/icon'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 describe('Bullseye Icon', () => {
   it('Renders', () => {
     render(<BullseyeIcon size="small" />)
+  })
+
+  it('Has the right class name', () => {
+    render(<BullseyeIcon size="small" />)
+
+    expect(screen.getByRole('img', { hidden: true })).toHaveClass('icon__bullseye')
+  })
+
+  it('Can be styled', () => {
+    const StyledIcon = styled(BullseyeIcon)`
+      color: pink;
+    `
+
+    render(<StyledIcon size="small" data-testid="123abc" />)
+
+    expect(screen.getByTestId('123abc')).toHaveStyle({ color: 'pink' })
+  })
+
+  it('Can be given html attributes', () => {
+    render(<BullseyeIcon size="small" data-testid="123abc" title="some-title" />)
+
+    expect(screen.getByTestId('123abc')).toHaveAttribute('title', 'some-title')
   })
 })
