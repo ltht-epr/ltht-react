c6f9e9552d08ccaf99f24a404b3028787c525ad5
Muhammad Saqib <muhammad.saqib@airelogic.com>
2022-10-28 17:48:46 +0100

    react table vertical grouping feature

---
+++ packages/storybook/src/ui/atoms/icons/checkbox-icon.stories.tsx
@@ -0,0 +1,13 @@
+import { Story } from '@storybook/react'
+import { CheckboxIcon } from '@ltht-react/icon'
+
+// eslint-disable-next-line import/prefer-default-export
+export const Checkbox: Story = () => (
+  <>
+    <CheckboxIcon size="small" />
+    <br />
+    <CheckboxIcon size="medium" />
+    <br />
+    <CheckboxIcon size="large" />
+  </>
+)
