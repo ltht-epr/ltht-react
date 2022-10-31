68a4a6a3f2daeb90c58f23250b788b230572ae23
Sam Biram <samuel.biram@airelogic.com>
2022-10-31 15:44:30 +0000

    Create Icon Button

---
+++ components/styled/icon/src/organisms/icon-button.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled'
+import { FC, HTMLAttributes } from 'react'
+import Icon, { IconProps } from '../../lib/molecules/icon'
+
+const StyledInvisibleButton = styled.button`
+  border-width: 0;
+  background-color: rgba(0, 0, 0, 0);
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`
+
+const StyledFoo = styled.span`
+  margin-left: 0.4rem;
+`
+
+const IconButton: FC<Props> = ({ iconProps, text, ...rest }) => {
+  return (
+    <StyledInvisibleButton {...rest}>
+      <Icon {...iconProps} />
+      {text && <StyledFoo>{text}</StyledFoo>}
+    </StyledInvisibleButton>
+  )
+}
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  iconProps: IconProps
+  text?: string
+}
+
+export default IconButton
---
+++ packages/storybook/src/ui/organisms/icon-button/icon-button.stories.tsx
@@ -0,0 +1,40 @@
+import IconButton from '@ltht-react/icon/src/organisms/icon-button'
+import { Story } from '@storybook/react'
+import { useState } from 'react'
+
+export const IconButtons: Story = () => {
+  const [timesClicked, setTimesClicked] = useState<number>(0)
+
+  const clickHandler = () => {
+    console.log('I got clicked!')
+    setTimesClicked(timesClicked + 1)
+  }
+
+  return (
+    <>
+      <div style={{ marginBottom: '1rem' }}>These buttons have been clicked {timesClicked} times</div>
+
+      <div style={{ marginBottom: '1rem' }}>
+        <IconButton iconProps={{ type: 'square', size: 'large' }} onClick={clickHandler}></IconButton>
+      </div>
+
+      <div style={{ marginBottom: '1rem' }}>
+        <IconButton
+          iconProps={{ type: 'info-circle', status: 'red', size: 'large' }}
+          onClick={clickHandler}
+          text="Click me!"
+        ></IconButton>
+      </div>
+
+      <div style={{ marginBottom: '1rem' }}>
+        <IconButton
+          iconProps={{ type: 'calendar', size: 'large' }}
+          text="Some button text"
+          onClick={clickHandler}
+        ></IconButton>
+      </div>
+    </>
+  )
+}
+
+export default { title: 'UI/Atoms/Buttons' }
