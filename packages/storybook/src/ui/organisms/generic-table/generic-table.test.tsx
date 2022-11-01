fb1ceb8aa0e99cefd4be15973aee9ff1179853cf
Sam Biram <samuel.biram@airelogic.com>
2022-11-01 22:34:56 +0000

    Add some more generic table tests

--- packages/storybook/src/ui/organisms/generic-table/generic-table.test.tsx
+++ packages/storybook/src/ui/organisms/generic-table/generic-table.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { GenericTable } from '@ltht-react/table'
+import Table, { GenericTable } from '@ltht-react/table'
 import {
   mockMappingMethodHorizontalWithCellCustomisation,
   mockMappingMethodHorizontalWithHeaderCustomisation,
@@ -9,6 +9,52 @@ import {
 } from './generic-table.mockdata'
 
 describe('Generic Table', () => {
+  it('Renders', () => {
+    render(
+      <GenericTable
+        headerAxis="x"
+        columnData={mockSummaryDefinition}
+        rowData={mockSummaryRecordsList}
+        mapToTableData={mockMappingMethodHorizontalWithCellCustomisation}
+      />
+    )
+  })
+
+  it('Calls the mapping method with the data given', () => {
+    const mockMappingMethod = jest.fn().mockImplementation(mockMappingMethodHorizontalWithCellCustomisation)
+
+    render(
+      <GenericTable
+        headerAxis="x"
+        columnData={mockSummaryDefinition}
+        rowData={mockSummaryRecordsList}
+        mapToTableData={mockMappingMethod}
+      />
+    )
+
+    expect(mockMappingMethod).toHaveBeenCalledTimes(1)
+    expect(mockMappingMethod).toHaveBeenCalledWith(mockSummaryDefinition, mockSummaryRecordsList)
+  })
+
+  it('Renders the same HTML as it would if given the TableData directly', () => {
+    const tableRenderedWithMapper = render(
+      <GenericTable
+        headerAxis="x"
+        columnData={mockSummaryDefinition}
+        rowData={mockSummaryRecordsList}
+        mapToTableData={mockMappingMethodHorizontalWithCellCustomisation}
+      />
+    ).asFragment()
+
+    const tableRenderedWithTableData = render(
+      <Table
+        tableData={mockMappingMethodHorizontalWithCellCustomisation(mockSummaryDefinition, mockSummaryRecordsList)}
+      />
+    ).asFragment()
+
+    expect(tableRenderedWithMapper).toEqual(tableRenderedWithTableData)
+  })
+
   it('Renders horizontally with cell customisation', () => {
     render(
       <GenericTable
