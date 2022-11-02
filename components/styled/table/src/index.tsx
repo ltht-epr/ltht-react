17345106f4956fc9f841034ac0a20ad4d3ccac9c
Sam Biram <samuel.biram@airelogic.com>
2022-11-02 09:12:58 +0000

    Remove tablecell atom for now - can be its own PR

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -1,10 +1,8 @@
-import TableCell from './atoms/table-cell'
 import Table, { ICellProps, Header, Cell, CellRow, TableData, ReactTableCell } from './molecules/table'
 import GenericTable from './organisms/generic-table'
 import QuestionnaireTable from './organisms/questionnaire-table'
 
 export default Table
-export { TableCell }
 export { ICellProps, Header, Cell, CellRow, TableData, ReactTableCell }
 export { GenericTable }
 export { QuestionnaireTable }
