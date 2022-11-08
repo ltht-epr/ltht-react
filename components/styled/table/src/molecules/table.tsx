a0292bc0a97a1609d82a78608aa55aba869848c0
airemechak <mechak.holondo@nhs.net>
2022-11-08 14:21:26 +0000

    Fixed linting issues

--- components/styled/table/src/molecules/table.tsx
+++ components/styled/table/src/molecules/table.tsx
@@ -13,9 +13,9 @@ import {
 } from '@tanstack/react-table'
 import uuid from 'react-uuid'
 import styled from '@emotion/styled'
-import {CSS_RESET,TRANSLUCENT_MID_GREY,SCROLLBAR,TRANSLUCENT_BRIGHT_BLUE} from '@ltht-react/styles'
+import { CSS_RESET, TRANSLUCENT_MID_GREY, SCROLLBAR, TRANSLUCENT_BRIGHT_BLUE } from '@ltht-react/styles'
 
-const Container =styled.div`
+const Container = styled.div`
   ${CSS_RESET};
   background-color: white;
   border-radius: 8px;
@@ -34,24 +34,24 @@ const Container =styled.div`
   }
 `
 
-const StyledTable =styled.table`
+const StyledTable = styled.table`
   background-color: white;
   border-collapse: collapse;
   border-radius: 8px;
   padding: 1rem;
 `
 
-const StyledTableHeader =styled.th`
+const StyledTableHeader = styled.th`
   border: thin solid rgba(200, 200, 200, 0.5);
   font-weight: bold;
   padding: 1rem;
 `
 
-const StyledTableBody =styled.tbody`
+const StyledTableBody = styled.tbody`
   text-align: center;
 `
 
-const StyledTableRow =styled.tr`
+const StyledTableRow = styled.tr`
   &:nth-of-type(odd) {
     background-color: ${TRANSLUCENT_MID_GREY};
   }
@@ -61,7 +61,7 @@ const StyledTableRow =styled.tr`
   }
 `
 
-const StyledTableData =styled.td`
+const StyledTableData = styled.td`
   border: thin solid rgba(200, 200, 200, 0.5);
   white-space: nowrap;
 
@@ -73,71 +73,71 @@ const StyledTableData =styled.td`
 const columnHelper = createColumnHelper<DataRow>()
 
 const generateColumnsFromHeadersRecursively = (headers?: Header[]): ColumnDef<DataRow, CellData | unknown>[] =>
-    headers?.map((header) =>
-        header.subheaders
-            ? columnHelper.group({
-              id: header.id ?? uuid(),
-              header: () => header.header,
-              columns: generateColumnsFromHeadersRecursively(header.subheaders),
-            })
-            : (columnHelper.accessor(header.accessor, {
-              id: header.accessor,
-              cell: (info) => {
-                const stringValue = (info?.getValue() as string) ?? ''
-                return header.cell
-                    ? header.cell({ value: stringValue, columnId: info.column.id, row: info.row.original })
-                    : stringValue
-              },
-              header: () => header.header,
-            }) as ColumnDef<DataRow, CellData | unknown>)
-    ) ?? []
+  headers?.map((header) =>
+    header.subheaders
+      ? columnHelper.group({
+          id: header.id ?? uuid(),
+          header: () => header.header,
+          columns: generateColumnsFromHeadersRecursively(header.subheaders),
+        })
+      : (columnHelper.accessor(header.accessor, {
+          id: header.accessor,
+          cell: (info) => {
+            const stringValue = (info?.getValue() as string) ?? ''
+            return header.cell
+              ? header.cell({ value: stringValue, columnId: info.column.id, row: info.row.original })
+              : stringValue
+          },
+          header: () => header.header,
+        }) as ColumnDef<DataRow, CellData | unknown>)
+  ) ?? []
 
 const generateRowsFromCellRows = (cellRows: CellRow[]): DataRow[] =>
-    cellRows.map((cellRow) => {
-      const mappedCell: DataRow = { subRows: [] }
-      const mappedCellRender: Record<string, FC<ICellProps>> = {}
+  cellRows.map((cellRow) => {
+    const mappedCell: DataRow = { subRows: [] }
+    const mappedCellRender: Record<string, FC<ICellProps>> = {}
 
-      cellRow.cells.forEach((cell) => {
-        mappedCell[cell.key] = cell.value
-        if (cell.render) {
-          mappedCellRender[cell.key] = cell.render
-        }
-      })
-
-      return {
-        ...mappedCell,
-        ...{
-          rowId: cellRow.id ?? '',
-          render: cellRow.render ? cellRow.render : (props: ICellProps) => <>{props.value}</>,
-          renderCells: mappedCellRender,
-        },
-        subRows: generateRowsFromCellRows(cellRow.subRows ?? []),
+    cellRow.cells.forEach((cell) => {
+      mappedCell[cell.key] = cell.value
+      if (cell.render) {
+        mappedCellRender[cell.key] = cell.render
       }
     })
 
+    return {
+      ...mappedCell,
+      ...{
+        rowId: cellRow.id ?? '',
+        render: cellRow.render ? cellRow.render : (props: ICellProps) => <>{props.value}</>,
+        renderCells: mappedCellRender,
+      },
+      subRows: generateRowsFromCellRows(cellRow.subRows ?? []),
+    }
+  })
+
 const getExpanderColumn = (): ColumnDef<DataRow, CellData | unknown> =>
-    columnHelper.accessor('expander', {
-      header: ({ table }) => (
-          <span
-              title="Toggle All Rows Expanded"
-              onClick={table.getToggleAllRowsExpandedHandler()}
-              style={{ cursor: 'pointer' }}
-          >
+  columnHelper.accessor('expander', {
+    header: ({ table }) => (
+      <span
+        title="Toggle All Rows Expanded"
+        onClick={table.getToggleAllRowsExpandedHandler()}
+        style={{ cursor: 'pointer' }}
+      >
         {table.getIsAllRowsExpanded() ? '▲' : '►'}
       </span>
-      ),
-      cell: ({ row }) =>
-          row.getCanExpand() ? (
-              <span
-                  {...{
-                    onClick: row.getToggleExpandedHandler(),
-                    style: { cursor: 'pointer', paddingLeft: `${row.depth * 2}rem` },
-                  }}
-              >
+    ),
+    cell: ({ row }) =>
+      row.getCanExpand() ? (
+        <span
+          {...{
+            onClick: row.getToggleExpandedHandler(),
+            style: { cursor: 'pointer', paddingLeft: `${row.depth * 2}rem` },
+          }}
+        >
           {row.getIsExpanded() ? '▲' : '►'}
         </span>
-          ) : null,
-    }) as ColumnDef<DataRow, CellData | unknown>
+      ) : null,
+  }) as ColumnDef<DataRow, CellData | unknown>
 
 export default function Table({ tableData }: IProps): JSX.Element {
   const [columns, setColumns] = useState<ColumnDef<DataRow, CellData | unknown>[]>([])
@@ -150,7 +150,7 @@ export default function Table({ tableData }: IProps): JSX.Element {
     const dataArray = generateRowsFromCellRows(tableData.rows)
 
     setColumns(
-        dataArray.some((x: DataRow) => x.subRows.length > 0) ? [getExpanderColumn(), ...columnArray] : columnArray
+      dataArray.some((x: DataRow) => x.subRows.length > 0) ? [getExpanderColumn(), ...columnArray] : columnArray
     )
     setData(dataArray)
   }, [tableData])
@@ -171,52 +171,52 @@ export default function Table({ tableData }: IProps): JSX.Element {
   })
 
   const getSortIcon = (sortDirection: SortDirection): string | null =>
-      ({
-        asc: ' 🔼',
-        desc: ' 🔽',
-      }[sortDirection] ?? null)
+    ({
+      asc: ' 🔼',
+      desc: ' 🔽',
+    }[sortDirection] ?? null)
 
   return (
-      <Container>
-        <StyledTable>
-          <thead>
+    <Container>
+      <StyledTable>
+        <thead>
           {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) =>
-                    header.column.id === 'expander' ? (
-                        <StyledTableHeader key={header.id} colSpan={header.colSpan}>
-                          {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                        </StyledTableHeader>
-                    ) : (
-                        <StyledTableHeader
-                            key={header.id}
-                            colSpan={header.colSpan}
-                            {...{
-                              style: {
-                                cursor: header.column.getCanSort() ? 'pointer' : '',
-                              },
-                              onClick: header.column.getToggleSortingHandler(),
-                            }}
-                        >
-                          {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                          {getSortIcon(header.column.getIsSorted() as SortDirection)}
-                        </StyledTableHeader>
-                    )
-                )}
-              </tr>
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) =>
+                header.column.id === 'expander' ? (
+                  <StyledTableHeader key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </StyledTableHeader>
+                ) : (
+                  <StyledTableHeader
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    {...{
+                      style: {
+                        cursor: header.column.getCanSort() ? 'pointer' : '',
+                      },
+                      onClick: header.column.getToggleSortingHandler(),
+                    }}
+                  >
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    {getSortIcon(header.column.getIsSorted() as SortDirection)}
+                  </StyledTableHeader>
+                )
+              )}
+            </tr>
           ))}
-          </thead>
-          <StyledTableBody>
-            {table.getRowModel().rows.map((row) => (
-                <StyledTableRow key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                      <StyledTableData>{flexRender(cell.column.columnDef.cell, cell.getContext())}</StyledTableData>
-                  ))}
-                </StyledTableRow>
-            ))}
-          </StyledTableBody>
-        </StyledTable>
-      </Container>
+        </thead>
+        <StyledTableBody>
+          {table.getRowModel().rows.map((row) => (
+            <StyledTableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <StyledTableData>{flexRender(cell.column.columnDef.cell, cell.getContext())}</StyledTableData>
+              ))}
+            </StyledTableRow>
+          ))}
+        </StyledTableBody>
+      </StyledTable>
+    </Container>
   )
 }
 
