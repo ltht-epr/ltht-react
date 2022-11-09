7977f0e2371888d94b49568c50949389e02f7b88
Jack <jackthorpe1@gmail.com>
2022-11-09 16:51:09 +0000

    Html input updates (#173)
    Update Text and Dropdown input components (PR #173)
    Co-authored by: @Moza805 
    Approved by: @sambiramairelogic

--- components/styled/select/src/index.tsx
+++ components/styled/select/src/index.tsx
@@ -1,99 +1,30 @@
-import { FC, HTMLAttributes, useEffect, useRef, useState } from 'react'
+import { FC, OptionHTMLAttributes, SelectHTMLAttributes } from 'react'
 import styled from '@emotion/styled'
-import { CSS_RESET, DESKTOP_MINIMUM_MEDIA_QUERY } from '@ltht-react/styles'
-import { ChevronIcon } from '@ltht-react/icon'
+import { inputBaseStyles } from '@ltht-react/styles'
 
-import Option, { IProps as OptionProps } from './atoms/option'
-
-const StyledSelect = styled.div`
-  ${CSS_RESET};
-  display: flex;
-  flex-direction: column;
-  position: relative;
-`
-
-const SelectTrigger = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
+const StyledSelect = styled.select`
+  ${inputBaseStyles}
+  height:100%;
   width: 100%;
-  min-height: 32px;
-  border: 1px solid black;
-  padding: 0.5rem 1rem;
-  background: white;
-  cursor: pointer;
+  color: black;
 
-  ${DESKTOP_MINIMUM_MEDIA_QUERY} {
-    min-height: 39px;
+  option:disabled {
+    display: none;
   }
 `
 
-const ActiveOption = styled.span`
-  font-weight: bold;
-  flex: 1;
-`
+const Select: FC<ISelectProps> = ({ onChange, options, value, ...rest }) => (
+  <StyledSelect required onChange={(e) => onChange && onChange(e)} value={value} {...rest}>
+    {options.map((item, key) => (
+      <option key={key} value={item.value}>
+        {item.label}
+      </option>
+    ))}
+  </StyledSelect>
+)
 
-const SelectOptions = styled.div<ISelectOptions>`
-  width: 100%;
-  display: ${({ open }) => (open ? 'block' : 'none')};
-  position: absolute;
-  top: 100%;
-  left: 0;
-`
-
-const Select: FC<IProps> & SelectComposition = ({ children, activeOption = '', ...rest }) => {
-  const [open, setOpen] = useState(false)
-  const selectRef = useRef<HTMLDivElement>(null)
-
-  const handleOnEscapeKey = (e: KeyboardEvent) => {
-    // IE11 has the key of 'Esc'
-    if (e.key === 'Escape' || e.key === 'Esc') {
-      setOpen(false)
-    }
-  }
-
-  const handleOutsideClick = (e: MouseEvent) => {
-    if (selectRef.current && !selectRef.current.contains(e.target as Node)) {
-      setOpen(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleOnEscapeKey, true)
-    document.addEventListener('click', handleOutsideClick, true)
-
-    // Cleanup
-    return () => {
-      document.removeEventListener('keydown', handleOnEscapeKey, true)
-      document.removeEventListener('click', handleOutsideClick, true)
-    }
-  })
-
-  return (
-    <StyledSelect {...rest} ref={selectRef}>
-      <SelectTrigger data-testid="select-trigger" onClick={() => setOpen((open) => !open)}>
-        <ActiveOption data-testid="select-active-option">{activeOption}</ActiveOption>
-        <ChevronIcon direction={open ? 'up' : 'down'} size="small" />
-      </SelectTrigger>
-      <SelectOptions data-testid="select-options" open={open} onClick={() => setOpen(false)}>
-        {children}
-      </SelectOptions>
-    </StyledSelect>
-  )
-}
-
-Select.Option = Option
-
-interface SelectComposition {
-  Option: FC<OptionProps>
-}
-
-interface IProps extends HTMLAttributes<HTMLDivElement> {
-  activeOption?: string
-}
-
-interface ISelectOptions {
-  open: boolean
+interface ISelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: OptionHTMLAttributes<HTMLOptionElement>[]
 }
 
 export default Select
