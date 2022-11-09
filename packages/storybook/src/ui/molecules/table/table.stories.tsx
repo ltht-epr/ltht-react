e5a2fd8697e17b7c7ce00997675e51d393109255
saqib-airelogic <muhammad.saqib@airelogic.com>
2022-11-09 18:42:27 +0000

    added few more examples

--- packages/storybook/src/ui/molecules/table/table.stories.tsx
+++ packages/storybook/src/ui/molecules/table/table.stories.tsx
@@ -1,6 +1,11 @@
 import Table from '@ltht-react/table'
 import { Story } from '@storybook/react'
-import { mockTableData, mockTableDataWithSubheaders, mockTableDataWithSubrows } from './table.mockdata'
+import {
+  mockTableData,
+  mockTableDataForPagination,
+  mockTableDataWithSubheaders,
+  mockTableDataWithSubrows,
+} from './table.mockdata'
 
 export const SimpleTable: Story = () => <Table tableData={mockTableData} />
 
@@ -14,6 +19,33 @@ export const TableWithScrollbar: Story = () => (
   <div style={{ maxWidth: '200px', height: '100px' }}>
     <Table tableData={mockTableData} />
   </div>
+)
+
+export const SimplePaginatedTable: Story = () => (
+  <Table tableData={mockTableDataForPagination} tableOptions={{ enablePagination: true }} />
+)
+
+export const SimpleServerSidePaginatedTable: Story = () => (
+  <Table
+    tableData={mockTableDataForPagination}
+    tableOptions={{ enablePagination: true, serverSidePagination: true }}
+    fetchData={async (options) => {
+      // Simulate some network latency
+      await new Promise((r) => setTimeout(r, 500))
+      return new Promise((res) =>
+        res({
+          tableData: {
+            headers: mockTableDataForPagination.headers,
+            rows: mockTableDataForPagination.rows.slice(
+              options.pageIndex * options.pageSize,
+              (options.pageIndex + 1) * options.pageSize
+            ),
+          },
+          totalCount: mockTableDataForPagination.rows.length,
+        })
+      )
+    }}
+  />
 )
 
 export default { title: 'UI/Molecules/Table' }
