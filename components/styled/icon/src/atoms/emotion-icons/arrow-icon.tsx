442001028f85f3820a69c1f72cec40332ce8fae5
saqib-airelogic <muhammad.saqib@airelogic.com>
2022-11-11 12:43:24 +0000

    changed expander arrows to iconbutton

---
+++ components/styled/icon/src/atoms/emotion-icons/arrow-icon.tsx
@@ -0,0 +1,33 @@
+import { FC, HTMLAttributes } from 'react'
+import { calculateEmotionIconSize, IconDirection, IconSize } from '@ltht-react/styles'
+import { DownArrow, UpArrow, LeftArrow, RightArrow } from '@emotion-icons/boxicons-solid'
+
+const calculateIconType = (direction: IconDirection): IconTypes => {
+  switch (direction) {
+    default:
+    case 'up':
+      return UpArrow
+    case 'down':
+      return DownArrow
+    case 'left':
+      return LeftArrow
+    case 'right':
+      return RightArrow
+  }
+}
+
+const ArrowIcon: FC<Props> = ({ size, direction, className, ...rest }) => {
+  const Icon = calculateIconType(direction)
+  return (
+    <Icon className={`${className ?? ''} icon__arrow`.trimStart()} size={calculateEmotionIconSize(size)} {...rest} />
+  )
+}
+
+type IconTypes = typeof DownArrow | typeof UpArrow | typeof LeftArrow | typeof RightArrow
+
+interface Props extends HTMLAttributes<SVGElement> {
+  size: IconSize
+  direction: IconDirection
+}
+
+export default ArrowIcon
