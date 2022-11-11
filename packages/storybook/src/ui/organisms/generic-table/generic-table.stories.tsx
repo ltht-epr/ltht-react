a179330dbc21645a8d6efb13c5fa8b376df0e9b0
saqib-airelogic <muhammad.saqib@airelogic.com>
2022-11-11 15:54:57 +0000

    bad merge

--- packages/storybook/src/ui/organisms/generic-table/generic-table.stories.tsx
+++ packages/storybook/src/ui/organisms/generic-table/generic-table.stories.tsx
@@ -100,41 +100,4 @@ export const MockDataVisualisationVerticalWithPagination: Story = () => (
   </>
 )
 
-export const MockDataVisualisationHorizontalWithClientSidePagination: Story = () => (
-  <GenericTable
-    headerAxis="x"
-    columnData={mockSummaryDefinition}
-    rowData={mockSummaryRecordsListForPagination}
-    mapToTableData={mockMappingMethodHorizontalWithCellCustomisation}
-    tableOptions={{ enablePagination: true, pageSize: 5, perPageOptions: [5, 10, 15, 20] }}
-  />
-)
-
-export const MockDataVisualisationVerticalWithPagination: Story = () => (
-  <GenericTable
-    headerAxis="y"
-    columnData={mockSummaryDefinition}
-    rowData={mockSummaryRecordsListForPagination}
-    mapToTableData={mockMappingMethodVerticalWithCellCustomisation}
-    tableOptions={{
-      enablePagination: true,
-      serverSidePagination: true,
-      pageSize: 1,
-      perPageOptions: [1, 10, 20, 50, 100],
-    }}
-    fetchData={(options) =>
-      new Promise((res) => {
-        res({
-          columnData: mockSummaryDefinition,
-          rowData: mockSummaryRecordsListForPagination.slice(
-            options.pageIndex * options.pageSize,
-            (options.pageIndex + 1) * options.pageSize
-          ),
-          totalCount: mockSummaryRecordsListForPagination.length,
-        })
-      })
-    }
-  />
-)
-
 export default { title: 'UI/Organisms/GenericTable' }
