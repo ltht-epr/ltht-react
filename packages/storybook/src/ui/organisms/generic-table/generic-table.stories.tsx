8db7e7453281c054e39dbba2ce36e7c7afe540f7
Sam Biram <samuel.biram@airelogic.com>
2022-11-14 14:30:36 +0000

    Missed one extra headerAxis

--- packages/storybook/src/ui/organisms/generic-table/generic-table.stories.tsx
+++ packages/storybook/src/ui/organisms/generic-table/generic-table.stories.tsx
@@ -3,12 +3,7 @@ import { GenericTable } from '@ltht-react/table'
 import { mockMapper, mockSummaryDefinition, mockSummaryRecordsList } from './generic-table.mockdata'
 
 export const TableCreatedWithGenericData: Story = () => (
-  <GenericTable
-    headerAxis="y"
-    columnData={mockSummaryDefinition}
-    rowData={mockSummaryRecordsList}
-    mapToTableData={mockMapper}
-  />
+  <GenericTable columnData={mockSummaryDefinition} rowData={mockSummaryRecordsList} mapToTableData={mockMapper} />
 )
 
 export default { title: 'UI/Organisms/GenericTable' }
