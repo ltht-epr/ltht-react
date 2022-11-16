d317d01e45a2de5c3dc224a0956879f3604c1266
Sam Biram <samuel.biram@airelogic.com>
2022-11-16 16:21:10 +0000

    Remove any

--- components/styled/table/src/molecules/useResize.tsx
+++ components/styled/table/src/molecules/useResize.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react'
 
-// eslint-disable-next-line
-const useResize = (myRef: React.RefObject<any>) => {
+const useResize = (myRef: React.RefObject<HTMLElement>) => {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
 
   useEffect(() => {
     const getDimensions = () => ({
-      width: (myRef && myRef.current.offsetWidth) || 0,
-      height: (myRef && myRef.current.offsetHeight) || 0,
+      width: (myRef && myRef.current && myRef.current.offsetWidth) || 0,
+      height: (myRef && myRef.current && myRef.current.offsetHeight) || 0,
     })
 
     const handleResize = () => {
