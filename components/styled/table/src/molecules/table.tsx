a1b3ff6b05831a16b7eaf3b02a2e64769385ef6d
Sam Biram <samuel.biram@airelogic.com>
2022-11-16 14:34:22 +0000

    Abstract repeated styles

--- components/styled/table/src/molecules/table.tsx
+++ components/styled/table/src/molecules/table.tsx
@@ -1,3 +1,4 @@
+/* eslint no-nested-ternary: 0 */
 import { FC, useRef, useState } from 'react'
 import {
   flexRender,
@@ -40,54 +41,43 @@ const StyledTable = styled.table`
   border-radius: 6px;
 `
 
+const StyledTableHeader = styled.th<IStyledTableCell>`
+  background-color: ${TABLE_COLOURS.HEADER};
+  border: thin solid ${TABLE_COLOURS.BORDER};
+  font-weight: bold;
+  padding: 1rem;
+
+  ${({ stickyWidth }) =>
+    stickyWidth !== undefined
+      ? `
+      position: sticky !important;
+      left: ${stickyWidth}px;
+      top: 0;
+      z-index: 1;`
+      : ``}
+`
+
+const StyledTableData = styled.td<IStyledTableCell>`
+  border: thin solid ${TABLE_COLOURS.BORDER};
+  white-space: nowrap;
+
+  ${({ stickyWidth }) =>
+    stickyWidth !== undefined
+      ? `
+    position: sticky !important;
+    left: ${stickyWidth}px;
+    top: 0;
+    z-index: 1;`
+      : ``}
+
+  &:first-of-type {
+    background-color: ${TABLE_COLOURS.HEADER} !important;
+  }
+`
+
 const Table: FC<IProps> = ({ tableData, stickColumns = 1 }) => {
   const firstColumn = useRef(null)
   const { width } = useResize(firstColumn)
-
-  const StyledTableHeader = styled.th`
-    background-color: ${TABLE_COLOURS.HEADER};
-    border: thin solid ${TABLE_COLOURS.BORDER};
-    font-weight: bold;
-    padding: 1rem;
-
-    &.sticky {
-      position: sticky !important;
-      left: 0;
-      top: 0;
-      z-index: 1;
-    }
-
-    &.stickySecond {
-      position: sticky !important;
-      left: ${width}px;
-      top: 0;
-      z-index: 1;
-    }
-  `
-
-  const StyledTableData = styled.td`
-    border: thin solid ${TABLE_COLOURS.BORDER};
-    white-space: nowrap;
-    &:first-of-type {
-      background-color: ${TABLE_COLOURS.HEADER} !important;
-    }
-
-    &.sticky {
-      position: sticky !important;
-      left: 0;
-      top: 0;
-      z-index: 1;
-      background-color: white;
-    }
-
-    &.stickySecond {
-      position: sticky !important;
-      left: ${width}px;
-      top: 0;
-      z-index: 1;
-      background-color: white;
-    }
-  `
 
   const [expanded, setExpanded] = useState<ExpandedState>({})
   const [sorting, setSorting] = useState<SortingState>([])
@@ -115,11 +105,17 @@ const Table: FC<IProps> = ({ tableData, stickColumns = 1 }) => {
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header, headerIndex) =>
                 headerIndex === 0 ? (
-                  <StyledTableHeader key={header.id} colSpan={header.colSpan} ref={firstColumn} className="sticky">
+                  <StyledTableHeader
+                    stickyWidth={stickColumns ? 0 : undefined}
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    ref={firstColumn}
+                  >
                     {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
                   </StyledTableHeader>
                 ) : (
                   <StyledTableHeader
+                    stickyWidth={headerIndex < stickColumns ? width : undefined}
                     key={header.id}
                     colSpan={header.colSpan}
                     {...{
@@ -128,7 +124,6 @@ const Table: FC<IProps> = ({ tableData, stickColumns = 1 }) => {
                       },
                       onClick: header.column.getToggleSortingHandler(),
                     }}
-                    className={headerIndex < stickColumns ? 'stickySecond' : ''}
                   >
                     {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
                   </StyledTableHeader>
@@ -142,12 +137,12 @@ const Table: FC<IProps> = ({ tableData, stickColumns = 1 }) => {
             <tr key={row.id}>
               {row.getVisibleCells().map((cell, cellIdx) => (
                 <StyledTableData
+                  stickyWidth={cellIdx < stickColumns ? (cellIdx === 0 ? 0 : width) : undefined}
                   key={cell.id}
                   style={{
                     background: cellIdx % 2 === 1 ? TABLE_COLOURS.STRIPE_LIGHT : TABLE_COLOURS.STRIPE_DARK,
                     textAlign: 'center',
                   }}
-                  className={cellIdx < stickColumns ? (cellIdx === 0 ? 'sticky' : 'stickySecond') : ''}
                 >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </StyledTableData>
@@ -158,6 +153,10 @@ const Table: FC<IProps> = ({ tableData, stickColumns = 1 }) => {
       </StyledTable>
     </Container>
   )
+}
+
+interface IStyledTableCell {
+  stickyWidth?: number
 }
 
 export type DataEntity = Record<string, CellProps | DataEntity[]> & {
