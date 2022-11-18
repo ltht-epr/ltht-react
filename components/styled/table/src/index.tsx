a5498f5de03c3f46807ab8e9442c187be8c67e9e
Sam Biram <samuel.biram@airelogic.com>
2022-11-18 10:40:01 +0000

    Export AdminActions interface so ehr-client can use it

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -1,6 +1,7 @@
 import Table, { Header, DataEntity, TableData } from './molecules/table'
 import GenericTable from './organisms/generic-table'
 import QuestionnaireTable from './organisms/questionnaire-table'
+import { AdminActionsForQuestionnaire } from './organisms/questionnaire-table-methods'
 
 export default Table
-export { Header, TableData, DataEntity, GenericTable, QuestionnaireTable }
+export { Header, TableData, DataEntity, GenericTable, QuestionnaireTable, AdminActionsForQuestionnaire }
