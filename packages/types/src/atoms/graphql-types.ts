c21558a29d204d783cbb0e05921e93e02e416539
Jack <jackthorpe1@gmail.com>
2023-02-01 15:47:28 +0000

    Add care plan goal evaluation types to GraphQL (#217)
    Co-authored-by: @Moza805 
    Approved-by: @bl4cksm1th

--- packages/types/src/atoms/graphql-types.ts
+++ packages/types/src/atoms/graphql-types.ts
@@ -588,7 +588,20 @@ export type CarePlanContinuationType = {
 /** Record progress against a care plan through interventions and goals */
 export type CarePlanDocumentInput = {
   carePlanId?: Maybe<Scalars['Guid']>;
+  goalEvaluations?: Maybe<Array<Maybe<CarePlanGoalEvaluationInput>>>;
   interventions?: Maybe<Array<Maybe<CarePlanInterventionInput>>>;
+};
+
+/** Goal evaluations from the care plan */
+export type CarePlanGoalEvaluationInput = {
+  /** Provide a unique identifier so you can find the goal evaluation in the response object */
+  id?: Maybe<Scalars['Guid']>;
+  itemId?: Maybe<Scalars['String']>;
+  label?: Maybe<Scalars['String']>;
+  notes?: Maybe<Scalars['String']>;
+  performed?: Maybe<Scalars['DateTimeOffset']>;
+  type?: Maybe<CodingInput>;
+  value?: Maybe<CodingInput>;
 };
 
 export enum CarePlanIntentCode {
@@ -606,13 +619,13 @@ export type CarePlanInterventionActionInput = {
 
 /** Interventions performed from the care plan */
 export type CarePlanInterventionInput = {
-  actionDate?: Maybe<Scalars['DateTimeOffset']>;
   actions?: Maybe<Array<Maybe<CarePlanInterventionActionInput>>>;
-  /** Provide a unique identifier so you can find the outcome in the response object */
+  /** Provide a unique identifier so you can find the intervention in the response object */
   id?: Maybe<Scalars['Guid']>;
-  interventionId?: Maybe<Scalars['String']>;
+  itemId?: Maybe<Scalars['String']>;
   label?: Maybe<Scalars['String']>;
   notes?: Maybe<Scalars['String']>;
+  performed?: Maybe<Scalars['DateTimeOffset']>;
 };
 
 export enum CarePlanStatusCode {
@@ -1370,6 +1383,7 @@ export type EhrMutationDiscontinueCarePlanArgs = {
 /** Mutations of the LTHT EHR. */
 export type EhrMutationDocumentCarePlanArgs = {
   document: CarePlanDocumentInput;
+  goalEvaluationTemplate: Scalars['String'];
   interventionTemplate: Scalars['String'];
   patientGuid: Scalars['Guid'];
 };
