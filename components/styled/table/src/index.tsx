a7f4a04b02e55fe608a602ed16432e10dc99ecac
saqib-airelogic <muhammad.saqib@airelogic.com>
2023-06-14 16:26:28 +0100

    export missing from index file

--- components/styled/table/src/index.tsx
+++ components/styled/table/src/index.tsx
@@ -1,7 +1,7 @@
-import Table, { Header, DataEntity, TableData } from './molecules/table'
+import Table, { Header, DataEntity, TableData, CellProps } from './molecules/table'
 import GenericTable from './organisms/generic-table'
 import QuestionnaireTable from './organisms/questionnaire-table'
 import { AdminActionsForQuestionnaire } from './organisms/questionnaire-table-methods'
 
 export default Table
-export { Header, TableData, DataEntity, GenericTable, QuestionnaireTable, AdminActionsForQuestionnaire }
+export { Header, TableData, DataEntity, CellProps, GenericTable, QuestionnaireTable, AdminActionsForQuestionnaire }
