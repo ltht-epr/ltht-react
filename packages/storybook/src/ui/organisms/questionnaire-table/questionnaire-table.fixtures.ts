557f885da0073f0081565e822f359c5bff6c03d8
jsed500 <jsed500@googlemail.com>
2023-06-28 10:20:39 +0100

    New storybook data using real UAT2 axial (#256)

--- packages/storybook/src/ui/organisms/questionnaire-table/questionnaire-table.fixtures.ts
+++ packages/storybook/src/ui/organisms/questionnaire-table/questionnaire-table.fixtures.ts
@@ -10,7 +10,7 @@ import {
   QuestionnaireResponseStatus,
 } from '@ltht-react/types'
 
-export const summaryDefinitionItems: QuestionnaireItem[] = [
+export const obsDefinitionItems: QuestionnaireItem[] = [
   {
     text: 'Score',
     type: QuestionnaireItemTypeCode.QuestionString,
@@ -105,7 +105,7 @@ export const summaryDefinitionItems: QuestionnaireItem[] = [
   },
 ]
 
-export const summaryDefinition: Questionnaire = {
+export const obsDefinition: Questionnaire = {
   extension: [
     {
       url: 'https://leedsth.nhs.uk/cds/template',
@@ -137,10 +137,61 @@ export const summaryDefinition: Questionnaire = {
   status: QuestionnairePublicationStatus.Active,
   id: '1',
   title: 'Observations',
-  item: summaryDefinitionItems,
+  item: obsDefinitionItems,
 }
 
-const summaryRecordOne: QuestionnaireResponse = {
+export const axialDefinition: Questionnaire = {
+  identifier: [],
+  metadata: {
+    requestedWhen: '',
+    dataSources: [],
+    isRedacted: false,
+  },
+  id: 'spa-axial-proms',
+  url: null,
+  version: null,
+  title: 'Axial Questionnaire',
+  status: QuestionnairePublicationStatus.Active,
+  item: [
+    {
+      linkId: 'spine-pain-vas',
+      text: 'Spine Pain (VAS)',
+      type: QuestionnaireItemTypeCode.QuestionInteger,
+      repeats: null,
+      item: [],
+    },
+    {
+      linkId: 'global-spondylitis-gvas',
+      text: 'Global Spondylitis (GVAS)',
+      type: QuestionnaireItemTypeCode.QuestionInteger,
+      repeats: null,
+      item: [],
+    },
+    {
+      linkId: 'basdai-total',
+      text: 'BASDAI Total',
+      type: QuestionnaireItemTypeCode.QuestionDecimal,
+      repeats: null,
+      item: [],
+    },
+    {
+      linkId: 'average-basfi-score',
+      text: 'Average BASFI Score',
+      type: QuestionnaireItemTypeCode.QuestionDecimal,
+      repeats: null,
+      item: [],
+    },
+    {
+      linkId: 'created-by',
+      text: 'Created By',
+      type: QuestionnaireItemTypeCode.QuestionString,
+      repeats: null,
+      item: [],
+    },
+  ],
+}
+
+const obsRecordOne: QuestionnaireResponse = {
   authored: {
     value: '2022-02-17T17:23:56+00:00',
     kind: PartialDateTimeKindCode.DateTime,
@@ -384,7 +435,7 @@ const summaryRecordOne: QuestionnaireResponse = {
   ],
 }
 
-const summaryRecordTwo = {
+const obsRecordTwo = {
   authored: {
     value: '2022-02-12T12:33:00+00:00',
     kind: PartialDateTimeKindCode.DateTime,
@@ -628,7 +679,7 @@ const summaryRecordTwo = {
   ],
 }
 
-const summaryRecordThree = {
+const obsRecordThree = {
   authored: {
     value: '2022-01-01T16:02:00+00:00',
     kind: PartialDateTimeKindCode.DateTime,
@@ -872,7 +923,527 @@ const summaryRecordThree = {
   ],
 }
 
-export const adminActionsForForms: AdminActionsForQuestionnaire[] = [
+const axialRecordOne: QuestionnaireResponse = {
+  id: '75f131eb-5cb8-47b7-a12c-2fa71bf4bbc7',
+  status: QuestionnaireResponseStatus.Completed,
+  authored: {
+    kind: PartialDateTimeKindCode.DateTime,
+    value: '2023-06-23T14:14:02.043\u002B00:00',
+  },
+  author: {
+    typeName: 'EhrUser',
+    identifier: {
+      system: null,
+      value: '2a6bd0df-1d86-4cf5-af55-7c3987e3f5bf',
+    },
+    display: 'TODO: populate this value',
+  },
+  item: [
+    {
+      linkId: 'spine-pain-vas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 4,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'global-spondylitis-gvas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 7,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'basdai-total',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'average-basfi-score',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 4.4,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'created-by',
+      text: null,
+      answer: [
+        {
+          valueString: 'WELSH, Thomas (Brigadier) Developer - Drama Therapy',
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+  ],
+  metadata: {
+    isRedacted: false,
+    dataSources: [],
+    requestedWhen: '',
+  },
+  text: {
+    text: 'Axial Questionnaire',
+    div: '',
+  },
+  extension: [
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-status-code',
+      valueCoding: {
+        code: 'Active',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/template-name',
+      valueCoding: {
+        code: 'spa-axial-proms',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-set-identifier',
+      valueCoding: null,
+      valueUuid: '555f92dd-9ac1-46d5-9dea-3de040fa6fc2',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-identifier',
+      valueCoding: null,
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: {
+        value: '75f131eb-5cb8-47b7-a12c-2fa71bf4bbc7',
+      },
+    },
+  ],
+}
+
+const axialRecordTwo: QuestionnaireResponse = {
+  id: '28e976ca-77b9-47b3-a093-4ccf8bfafac8',
+  status: QuestionnaireResponseStatus.Completed,
+  authored: {
+    kind: PartialDateTimeKindCode.DateTime,
+    value: '2023-06-23T13:37:27.82\u002B00:00',
+  },
+  author: {
+    typeName: 'EhrUser',
+    identifier: {
+      system: null,
+      value: '2a6bd0df-1d86-4cf5-af55-7c3987e3f5bf',
+    },
+    display: 'TODO: populate this value',
+  },
+  item: [
+    {
+      linkId: 'spine-pain-vas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 5,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'global-spondylitis-gvas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 5,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'basdai-total',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'average-basfi-score',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5.2,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'created-by',
+      text: null,
+      answer: [
+        {
+          valueString: 'WELSH, Thomas (Brigadier) Developer - Drama Therapy',
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+  ],
+  metadata: {
+    isRedacted: false,
+    dataSources: [],
+    requestedWhen: '',
+  },
+  text: {
+    text: 'Axial Questionnaire',
+    div: '',
+  },
+  extension: [
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-status-code',
+      valueCoding: {
+        code: 'Active',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/template-name',
+      valueCoding: {
+        code: 'spa-axial-proms',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-set-identifier',
+      valueCoding: null,
+      valueUuid: '1ef3a0d5-b2cc-41bf-8dcd-59c0448fe865',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-identifier',
+      valueCoding: null,
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: {
+        value: '28e976ca-77b9-47b3-a093-4ccf8bfafac8',
+      },
+    },
+  ],
+}
+
+const axialRecordThree: QuestionnaireResponse = {
+  id: '074d7347-964c-43fb-bb1b-08f0da7be3b6',
+  status: QuestionnaireResponseStatus.Completed,
+  authored: {
+    kind: PartialDateTimeKindCode.DateTime,
+    value: '2023-06-21T14:50:49.003\u002B00:00',
+  },
+  author: {
+    typeName: 'EhrUser',
+    identifier: {
+      system: null,
+      value: 'f68e4d28-beae-421f-842f-f6b5fe34e15f',
+    },
+    display: 'TODO: populate this value',
+  },
+  item: [
+    {
+      linkId: 'spine-pain-vas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 4,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'global-spondylitis-gvas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 4,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'basdai-total',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5.1,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'average-basfi-score',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5.2,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'created-by',
+      text: null,
+      answer: [
+        {
+          valueString: 'CATEGORY, Test (Mr) test5 - Accident and Emergency',
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+  ],
+  metadata: {
+    isRedacted: false,
+    dataSources: [],
+    requestedWhen: '',
+  },
+  text: {
+    text: 'Axial Questionnaire',
+    div: '',
+  },
+  extension: [
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-status-code',
+      valueCoding: {
+        code: 'Active',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/template-name',
+      valueCoding: {
+        code: 'spa-axial-proms',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-set-identifier',
+      valueCoding: null,
+      valueUuid: '0b3dc920-b125-4964-a3e2-ddc52c28774e',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-identifier',
+      valueCoding: null,
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: {
+        value: '074d7347-964c-43fb-bb1b-08f0da7be3b6',
+      },
+    },
+  ],
+}
+
+const axialRecordFour: QuestionnaireResponse = {
+  id: '451c890b-38a0-4fa9-ab71-dfba655c5f78',
+  status: QuestionnaireResponseStatus.Completed,
+  authored: {
+    kind: PartialDateTimeKindCode.DateTime,
+    value: '2023-06-20T13:24:07.957\u002B00:00',
+  },
+  author: {
+    typeName: 'EhrUser',
+    identifier: {
+      system: null,
+      value: '2e950fd3-e16b-4628-ac6f-a312af55df6c',
+    },
+    display: 'TODO: populate this value',
+  },
+  item: [
+    {
+      linkId: 'spine-pain-vas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 7,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'global-spondylitis-gvas',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: 4,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'basdai-total',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5.8,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'average-basfi-score',
+      text: null,
+      answer: [
+        {
+          valueString: null,
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: 5.8,
+          item: null,
+        },
+      ],
+    },
+    {
+      linkId: 'created-by',
+      text: null,
+      answer: [
+        {
+          valueString: 'ROBERTSON, Christopher (Mr) Senior Project Analyst',
+          valueBoolean: null,
+          valueDateTime: null,
+          valueInteger: null,
+          valueDecimal: null,
+          item: null,
+        },
+      ],
+    },
+  ],
+  metadata: {
+    isRedacted: false,
+    dataSources: [],
+    requestedWhen: '',
+  },
+  text: {
+    text: 'Axial Questionnaire',
+    div: '',
+  },
+  extension: [
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-status-code',
+      valueCoding: {
+        code: 'Active',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/template-name',
+      valueCoding: {
+        code: 'spa-peripheral-axial-proms',
+      },
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-set-identifier',
+      valueCoding: null,
+      valueUuid: 'b4a2714f-b205-4b1b-9f2f-7bb8333145ea',
+      valueIdentifier: null,
+    },
+    {
+      url: 'https://leedsth.nhs.uk/cds/instance-identifier',
+      valueCoding: null,
+      valueUuid: '00000000-0000-0000-0000-000000000000',
+      valueIdentifier: {
+        value: '451c890b-38a0-4fa9-ab71-dfba655c5f78',
+      },
+    },
+  ],
+}
+
+export const obsAdminActionsForForms: AdminActionsForQuestionnaire[] = [
   {
     questionnaire: '1',
     adminActions: [
@@ -959,4 +1530,126 @@ export const adminActionsForForms: AdminActionsForQuestionnaire[] = [
   },
 ]
 
-export const summaryRecordsList: QuestionnaireResponse[] = [summaryRecordOne, summaryRecordTwo, summaryRecordThree]
+export const obsRecordsList: QuestionnaireResponse[] = [obsRecordOne, obsRecordTwo, obsRecordThree]
+
+export const axialRecordsList: QuestionnaireResponse[] = [
+  axialRecordOne,
+  axialRecordTwo,
+  axialRecordThree,
+  axialRecordFour,
+]
+
+export const axialAdminActionsForForms: AdminActionsForQuestionnaire[] = [
+  {
+    questionnaire: '75f131eb-5cb8-47b7-a12c-2fa71bf4bbc7',
+    adminActions: [
+      {
+        text: 'View',
+        clickHandler: () => {
+          console.log('Viewing submission 1')
+        },
+        leftIcon: { type: 'info-circle', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Edit',
+        clickHandler: () => {
+          console.log('Editing submission 1')
+        },
+        leftIcon: { type: 'edit', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Delete',
+        clickHandler: () => {
+          console.log('Deleting submission 1')
+        },
+        leftIcon: { type: 'exclamation', size: 'medium' },
+      },
+    ],
+  },
+  {
+    questionnaire: '28e976ca-77b9-47b3-a093-4ccf8bfafac8',
+    adminActions: [
+      {
+        text: 'View',
+        clickHandler: () => {
+          console.log('Viewing submission 2')
+        },
+        leftIcon: { type: 'info-circle', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Edit',
+        clickHandler: () => {
+          console.log('Editing submission 2')
+        },
+        leftIcon: { type: 'edit', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Delete',
+        clickHandler: () => {
+          console.log('Deleting submission 2')
+        },
+        leftIcon: { type: 'exclamation', size: 'medium' },
+      },
+    ],
+  },
+  {
+    questionnaire: '074d7347-964c-43fb-bb1b-08f0da7be3b6',
+    adminActions: [
+      {
+        text: 'View',
+        clickHandler: () => {
+          console.log('Viewing submission 3')
+        },
+        leftIcon: { type: 'info-circle', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Edit',
+        clickHandler: () => {
+          console.log('Editing submission 3')
+        },
+        leftIcon: { type: 'edit', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Delete',
+        clickHandler: () => {
+          console.log('Deleting submission 3')
+        },
+        leftIcon: { type: 'exclamation', size: 'medium' },
+      },
+    ],
+  },
+  {
+    questionnaire: '451c890b-38a0-4fa9-ab71-dfba655c5f78',
+    adminActions: [
+      {
+        text: 'View',
+        clickHandler: () => {
+          console.log('Viewing submission 3')
+        },
+        leftIcon: { type: 'info-circle', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Edit',
+        clickHandler: () => {
+          console.log('Editing submission 3')
+        },
+        leftIcon: { type: 'edit', size: 'medium' },
+        rightIcon: { type: 'external-link', size: 'medium' },
+      },
+      {
+        text: 'Delete',
+        clickHandler: () => {
+          console.log('Deleting submission 3')
+        },
+        leftIcon: { type: 'exclamation', size: 'medium' },
+      },
+    ],
+  },
+]
--- packages/storybook/src/ui/organisms/questionnaire-table/questionnaire-table.stories.tsx
+++ packages/storybook/src/ui/organisms/questionnaire-table/questionnaire-table.stories.tsx
@@ -1,30 +1,64 @@
 import { Story } from '@storybook/react'
 import { QuestionnaireTable } from '@ltht-react/table'
-import { summaryDefinition, summaryRecordsList, adminActionsForForms } from './questionnaire-table.fixtures'
+import {
+  obsDefinition,
+  obsRecordsList,
+  obsAdminActionsForForms,
+  axialRecordsList,
+  axialDefinition,
+  axialAdminActionsForForms,
+} from './questionnaire-table.fixtures'
 
-export const VerticalTable: Story = () => (
-  <QuestionnaireTable definition={summaryDefinition} records={summaryRecordsList} staticColumns={2} />
+export const ObservationsVerticalTable: Story = () => (
+  <QuestionnaireTable definition={obsDefinition} records={obsRecordsList} staticColumns={2} />
 )
 
-export const VerticalTableWithAdminActions: Story = () => (
+export const AxialVerticalTable: Story = () => (
+  <QuestionnaireTable definition={axialDefinition} records={axialRecordsList} staticColumns={2} />
+)
+
+export const ObservationsVerticalTableWithAdminActions: Story = () => (
   <QuestionnaireTable
-    definition={summaryDefinition}
-    records={summaryRecordsList}
-    adminActions={adminActionsForForms}
+    definition={obsDefinition}
+    records={obsRecordsList}
+    adminActions={obsAdminActionsForForms}
     staticColumns={2}
   />
 )
 
-export const HorizontalTable: Story = () => (
-  <QuestionnaireTable definition={summaryDefinition} records={summaryRecordsList} headerAxis="x" staticColumns={2} />
+export const AxialVerticalTableWithAdminActions: Story = () => (
+  <QuestionnaireTable
+    definition={axialDefinition}
+    records={axialRecordsList}
+    adminActions={axialAdminActionsForForms}
+    staticColumns={2}
+  />
 )
 
-export const HorizontalTableAdminActions: Story = () => (
+export const ObservationsHorizontalTable: Story = () => (
+  <QuestionnaireTable definition={obsDefinition} records={obsRecordsList} headerAxis="x" staticColumns={2} />
+)
+
+export const AxialHorizontalTable: Story = () => (
+  <QuestionnaireTable definition={axialDefinition} records={axialRecordsList} headerAxis="x" staticColumns={2} />
+)
+
+export const ObservationsHorizontalTableAdminActions: Story = () => (
   <QuestionnaireTable
-    definition={summaryDefinition}
-    records={summaryRecordsList}
+    definition={obsDefinition}
+    records={obsRecordsList}
     headerAxis="x"
-    adminActions={adminActionsForForms}
+    adminActions={obsAdminActionsForForms}
+    staticColumns={2}
+  />
+)
+
+export const AxialHorizontalTableAdminActions: Story = () => (
+  <QuestionnaireTable
+    definition={axialDefinition}
+    records={axialRecordsList}
+    headerAxis="x"
+    adminActions={axialAdminActionsForForms}
     staticColumns={2}
   />
 )
