25e71af24cfa66e35eac20dc65b37d43ed7688e9
Sam Biram <samuel.biram@airelogic.com>
2023-09-21 15:29:56 +0100

    Create task assignee component

---
+++ components/clinical/task/src/atoms/task-assignee.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+import styled from '@emotion/styled'
+import { TEXT_COLOURS } from '@ltht-react/styles'
+
+const StyledDescription = styled.small`
+  color: ${TEXT_COLOURS.INFO}
+  flex: 1;
+`
+
+const Assignee: FC<IProps> = ({ assignedUser, assignedTeam }) => {
+  return <StyledDescription>{calculateAssigneeText(assignedUser, assignedTeam)}</StyledDescription>
+}
+
+const calculateAssigneeText = (assignedUser: string | undefined, assignedTeam: string | undefined) => {
+  if (!assignedUser && !assignedTeam) return null
+  if (assignedUser && assignedTeam) return `${assignedUser} | ${assignedTeam}`
+  return assignedUser ?? assignedTeam
+}
+
+interface IProps {
+  assignedUser?: string
+  assignedTeam?: string
+}
+
+export default Assignee
