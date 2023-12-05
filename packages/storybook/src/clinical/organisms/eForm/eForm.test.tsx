b6a12a0c11811b5132beafcf70365e136e955173
Mechak Holondo <mechak.holondo@airelogic.com>
2023-12-05 12:32:47 +0000

    Abstract the URL as it is irrelevant in test

--- packages/storybook/src/clinical/organisms/eForm/eForm.test.tsx
+++ packages/storybook/src/clinical/organisms/eForm/eForm.test.tsx
@@ -5,12 +5,10 @@ describe('eForm', () => {
   it('Increments iframeKey to trigger iframe refresh, when forceRefresh is true', () => {
     const mockCallback = jest.fn()
 
-    const { rerender } = render(
-      <EForm url="https://www.wikipedia.org/" callback={{ name: 'test', handler: mockCallback }} />
-    )
+    const { rerender } = render(<EForm url="https://example.com/" callback={{ name: 'test', handler: mockCallback }} />)
     const initialIframeKey = screen.getByTitle('eForm').getAttribute('data-iframe-key')
 
-    rerender(<EForm url="https://www.wikipedia.org/" callback={{ name: 'test', handler: mockCallback }} forceRefresh />)
+    rerender(<EForm url="https://example.com/" callback={{ name: 'test', handler: mockCallback }} forceRefresh />)
     const updatedIframeKey = screen.getByTitle('eForm').getAttribute('data-iframe-key')
 
     expect(updatedIframeKey).not.toBe(initialIframeKey)
@@ -19,12 +17,10 @@ describe('eForm', () => {
   it('Does not increment iframeKey, when forceRefresh is false', () => {
     const mockCallback = jest.fn()
 
-    const { rerender } = render(
-      <EForm url="https://www.wikipedia.org/" callback={{ name: 'test', handler: mockCallback }} />
-    )
+    const { rerender } = render(<EForm url="https://example.com/" callback={{ name: 'test', handler: mockCallback }} />)
     const initialIframeKey = screen.getByTitle('eForm').getAttribute('data-iframe-key')
 
-    rerender(<EForm url="https://www.wikipedia.org/" callback={{ name: 'test', handler: mockCallback }} />)
+    rerender(<EForm url="https://example.com/" callback={{ name: 'test', handler: mockCallback }} />)
     const updatedIframeKey = screen.getByTitle('eForm').getAttribute('data-iframe-key')
 
     expect(updatedIframeKey).toBe(initialIframeKey)
