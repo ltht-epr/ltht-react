60a885a2845770e882b7e36b7bc225915ec98bb4
Jack Thorpe <jack.thorpe@airelogic.com>
2024-01-11 12:14:26 +0000

    ignore dev imports in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,6 +51,12 @@ module.exports = {
         // '.test.ts': 'never',
       },
     ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.ts', '**/*.test.tsx'],
+      },
+    ],
   },
   settings: {
     'import/resolver': {
