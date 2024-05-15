0e806090f4785bd4298f9d5154ef37da387a91a4
Jack <jackthorpe1@gmail.com>
2024-05-15 17:06:16 +0100

    type:gen (#326)
    Co-authored-by: @Moza805 
    Approved-by: @Moza805

--- packages/types/src/atoms/graphql-types.ts
+++ packages/types/src/atoms/graphql-types.ts
@@ -1344,6 +1344,8 @@ export type Ehr = {
   tasks?: Maybe<TaskContinuation>;
   /** Returns the available CDS extension template for a given base template and value */
   templateExtension?: Maybe<Questionnaire>;
+  /** Gets template details such as display name and approval info. */
+  templateMetadata?: Maybe<Questionnaire>;
   /** Terminology */
   terminologyItems?: Maybe<TerminologyItemContinuation>;
 };
@@ -1704,6 +1706,13 @@ export type EhrTemplateExtensionArgs = {
   baseTemplateName?: Maybe<Scalars['String']>;
   baseTemplateVersion?: Maybe<Scalars['Int']>;
   extensionValue?: Maybe<Scalars['String']>;
+};
+
+
+/** Queries the LTHT EHR. */
+export type EhrTemplateMetadataArgs = {
+  templateName: Scalars['String'];
+  templateVersion?: Maybe<Scalars['Int']>;
 };
 
 
