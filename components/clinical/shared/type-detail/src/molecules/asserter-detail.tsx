cf329ea2ed159cd7fc2f32aaab64363cf99b474f
airemechak <mechak.holondo@nhs.net>
2024-08-05 12:35:53 +0100

    Update tests to match new element labels

--- components/clinical/shared/type-detail/src/molecules/asserter-detail.tsx
+++ components/clinical/shared/type-detail/src/molecules/asserter-detail.tsx
@@ -2,25 +2,23 @@ import { ConditionAsserter, Maybe } from '@ltht-react/types'
 import { NestedListDetail } from '.'
 import { DetailViewComponent, IDetailViewProps } from '../atoms/detail-view-component'
 
-const AsserterDetail: DetailViewComponent<IAsserterDetailProps> = ({ asserter, showIfEmpty = false }) => {
-  return (
+const AsserterDetail: DetailViewComponent<IAsserterDetailProps> = ({ asserter, showIfEmpty = false }) => (
+  <>
+    {(asserter || showIfEmpty) === true} && (
     <>
-      {(asserter || showIfEmpty) === true && (
-        <>
-          <NestedListDetail term="Diagnosis Stated By" showIfEmpty={showIfEmpty}>
-            {asserter?.asserterName}
-          </NestedListDetail>
-          <NestedListDetail term="Relationship" showIfEmpty={showIfEmpty}>
-            {asserter?.asserterType}
-          </NestedListDetail>
-          <NestedListDetail term="Practitioner Address/Organisation" showIfEmpty={showIfEmpty}>
-            {asserter?.asserterAddress}
-          </NestedListDetail>
-        </>
-      )}
+      <NestedListDetail term="Diagnosis Stated By" showIfEmpty={showIfEmpty}>
+        {asserter?.asserterName}
+      </NestedListDetail>
+      <NestedListDetail term="Relationship" showIfEmpty={showIfEmpty}>
+        {asserter?.asserterType}
+      </NestedListDetail>
+      <NestedListDetail term="Practitioner Address/Organisation" showIfEmpty={showIfEmpty}>
+        {asserter?.asserterAddress}
+      </NestedListDetail>
     </>
-  )
-}
+    )
+  </>
+)
 
 interface IAsserterDetailProps extends IDetailViewProps {
   asserter?: Maybe<ConditionAsserter> | null
