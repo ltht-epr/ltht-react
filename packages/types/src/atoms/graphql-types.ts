9ac7b1fcf971f5b9613885d8c691eac0d090fc6b
Hinchstarx <thomas.hinchliffe@nhs.net>
2024-09-20 14:20:45 +0100

    EHR-33201 Decision Context Create when editing FT

--- packages/types/src/atoms/graphql-types.ts
+++ packages/types/src/atoms/graphql-types.ts
@@ -2474,6 +2474,7 @@ export enum FeatureInstance {
   AssessmentRecordRelevantDiagnoses = 'ASSESSMENT_RECORD_RELEVANT_DIAGNOSES',
   ClinicOutcomePointInTime = 'CLINIC_OUTCOME_POINT_IN_TIME',
   ClinicOutcomeRelevantDiagnoses = 'CLINIC_OUTCOME_RELEVANT_DIAGNOSES',
+  DecisionContextCreateWhenEditingEnabled = 'DECISION_CONTEXT_CREATE_WHEN_EDITING_ENABLED',
   FilterAssignedTasks = 'FILTER_ASSIGNED_TASKS',
   FilterInactiveTasks = 'FILTER_INACTIVE_TASKS',
   FormsForHealthLaunch = 'FORMS_FOR_HEALTH_LAUNCH',
