4c818599001ef0e626dde0026c9fb139f8f2c37f
airemechak <mechak.holondo@nhs.net>
2024-11-26 17:01:52 +0000

    Replace diagnosis title text with snippet text value

--- packages/storybook/src/clinical/atoms/diagnosis/diagnosis-title.test.tsx
+++ packages/storybook/src/clinical/atoms/diagnosis/diagnosis-title.test.tsx
@@ -1,22 +1,36 @@
 import { render, screen } from '@testing-library/react'
 import DiagnosisTitle from '@ltht-react/diagnosis-summary/src/atoms/diagnosis-title'
-import { conditions } from '../../organisms/diagnosis/diagnosis.fixtures'
 
-const conditionWithClinicalCode = conditions[0]
-conditionWithClinicalCode.code = {
-  text: 'This is a clinical coding text summary',
-}
+const validTitle = 'Diagnosis Title Example'
 
 describe('Diagnosis Title', () => {
   it('Renders clinical code text summaries', () => {
-    render(<DiagnosisTitle condition={conditionWithClinicalCode} enteredInError={false} />)
+    render(<DiagnosisTitle text={validTitle} enteredInError={false} />)
 
-    expect(screen.getByText('This is a clinical coding text summary')).toBeVisible()
+    expect(screen.getByText('Diagnosis Title Example')).toBeVisible()
   })
 
   it('Adds the strikethrough property to code summaries if entered in error', () => {
-    render(<DiagnosisTitle condition={conditionWithClinicalCode} enteredInError />)
+    render(<DiagnosisTitle text={validTitle} enteredInError />)
 
-    expect(screen.getByText('This is a clinical coding text summary')).toHaveStyle('text-decoration: line-through')
+    expect(screen.getByText('Diagnosis Title Example')).toHaveStyle('text-decoration: line-through')
+  })
+
+  it('Displays missing data message when title text is empty', () => {
+    render(<DiagnosisTitle text="" enteredInError />)
+
+    expect(screen.getByText('Insufficient data provided.')).toBeVisible()
+  })
+
+  it('Displays missing data message when title text is null', () => {
+    render(<DiagnosisTitle text={null} enteredInError />)
+
+    expect(screen.getByText('Insufficient data provided.')).toBeVisible()
+  })
+
+  it('Displays missing data message when title text is undefined', () => {
+    render(<DiagnosisTitle text={undefined} enteredInError />)
+
+    expect(screen.getByText('Insufficient data provided.')).toBeVisible()
   })
 })
