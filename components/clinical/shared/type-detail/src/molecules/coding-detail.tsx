7df9f5178a7c2b5ffe4278e47b04c2dd37b9203c
airemechak <mechak.holondo@nhs.net>
2024-11-27 12:24:55 +0000

    Improve how coding-detail handles missing values

--- components/clinical/shared/type-detail/src/molecules/coding-detail.tsx
+++ components/clinical/shared/type-detail/src/molecules/coding-detail.tsx
@@ -3,13 +3,23 @@ import { Coding, Maybe } from '@ltht-react/types'
 import { DetailViewComponent, IDetailViewProps } from '../atoms/detail-view-component'
 import NestedListDetail from './nested-list-detail'
 
-const CodingDetail: DetailViewComponent<IProps> = ({ term, coding, showIfEmpty = false }) => (
-  <NestedListDetail term={term} showIfEmpty={showIfEmpty} wrapDescription={false}>
-    <DescriptionList.Description key={term}>
-      {coding?.display || 'Insufficient data provided.'}
-    </DescriptionList.Description>
-  </NestedListDetail>
-)
+const CodingDetail: DetailViewComponent<IProps> = ({ term, coding, showIfEmpty = false }) => {
+  let description
+
+  if (coding?.display) {
+    description = <DescriptionList.Description key={term}>{coding.display}</DescriptionList.Description>
+  } else if (showIfEmpty) {
+    description = <></>
+  } else {
+    description = <DescriptionList.Description key={term}>Insufficient data provided.</DescriptionList.Description>
+  }
+
+  return (
+    <NestedListDetail term={term} showIfEmpty={showIfEmpty} wrapDescription={false}>
+      {description}
+    </NestedListDetail>
+  )
+}
 
 interface IProps extends IDetailViewProps {
   term: string
