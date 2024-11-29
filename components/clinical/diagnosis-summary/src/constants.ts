bf9c10e7a5caef446680407c3652a488bac82d28
Mechak Holondo <mechakand1@gmail.com>
2024-11-29 16:49:30 +0000

    Add test for diagnosis-summary to ensure everything renders as expected

--- components/clinical/diagnosis-summary/src/constants.ts
+++ components/clinical/diagnosis-summary/src/constants.ts
@@ -1,3 +1,5 @@
 const SNIPPET_HOVER_TEXT = 'https://leedsth.nhs.uk/cds/snippet-hover-text'
+const EXTENSION_TEMPLATE_DISPLAY_NAME = 'https://leedsth.nhs.uk/cds/extension-template-display-name'
+const EXTENSION_TEMPLATE_VERSION = 'https://leedsth.nhs.uk/cds/extension-template-version'
 
-export default SNIPPET_HOVER_TEXT
+export { EXTENSION_TEMPLATE_DISPLAY_NAME, EXTENSION_TEMPLATE_VERSION, SNIPPET_HOVER_TEXT }
