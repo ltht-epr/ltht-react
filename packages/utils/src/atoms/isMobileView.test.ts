1d0e618e3c3b9da3196b523f511b70f27b3e2f3a
Jack Thorpe <jack.thorpe@airelogic.com>
2024-12-12 16:05:13 +0000

    Use constant on util functions

--- packages/utils/src/atoms/isMobileView.test.ts
+++ packages/utils/src/atoms/isMobileView.test.ts
@@ -1,13 +1,14 @@
+import { MOBILE_BREAKPOINT, TABLET_BREAKPOINT } from '@ltht-react/styles'
 import isMobileView from './isMobileView'
 
 describe('isMobileView', () => {
   it('Equal value is not mobile', () => {
-    expect(isMobileView(768)).toEqual(false)
+    expect(isMobileView(TABLET_BREAKPOINT.min)).toEqual(false)
   })
   it('Larger screen size not mobile', () => {
-    expect(isMobileView(769)).toEqual(false)
+    expect(isMobileView(TABLET_BREAKPOINT.min + 1)).toEqual(false)
   })
   it('Smaller screen size is mobile', () => {
-    expect(isMobileView(767)).toEqual(true)
+    expect(isMobileView(MOBILE_BREAKPOINT.max)).toEqual(true)
   })
 })
--- packages/utils/src/atoms/isMobileView.ts
+++ packages/utils/src/atoms/isMobileView.ts
@@ -1,5 +1,5 @@
-const MOBILE_BREAKPOINT = 768
+import { MOBILE_BREAKPOINT } from '@ltht-react/styles'
 
-const isMobileView = (width: number): boolean => width < MOBILE_BREAKPOINT
+const isMobileView = (width: number): boolean => width <= MOBILE_BREAKPOINT.max
 
 export default isMobileView
