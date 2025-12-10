de58035f30128f31efe1d8af55fc068916527140
Jack <jackthorpe1@gmail.com>
2025-12-10 11:49:38 +0000

    Guidance Action UI (#489)
    Guidance actions UI
    
    Co-authored-by: @Moza805
    Approved-by: @bl4cksm1th

--- packages/storybook/src/clinical/organisms/guidances/guidances.fixture.ts
+++ packages/storybook/src/clinical/organisms/guidances/guidances.fixture.ts
@@ -1,4 +1,4 @@
-import { Guidance, Metadata, GuidanceStatusCode } from '@ltht-react/types'
+import { Guidance, Metadata, GuidanceStatusCode, PartialDateTimeKindCode } from '@ltht-react/types'
 
 const mockMetadata: Metadata = {
   dataSources: [
@@ -76,6 +76,185 @@ const guidanceThree: Guidance = {
 }
 
 const guidanceList: Guidance[] = [guidanceOne, guidanceTwo, guidanceThree]
+
+export const guidanceWithActions: Guidance[] = [
+  {
+    id: '653e80c0-48e1-4421-8533-c78580ad2d44',
+    status: GuidanceStatusCode.Success,
+    occuranceDateTime: {
+      kind: PartialDateTimeKindCode.DateTime,
+      value: '2025-11-24T14:15:59.6348495+00:00',
+    },
+    requestIdentifier: {
+      value: '65917780-dd50-4eda-8494-69d491af9e9e',
+    },
+    reasonCode: {
+      text: 'Test Guidance',
+    },
+    text: {
+      div: '',
+      text: 'Add a care plan',
+    },
+    note: [
+      {
+        elementId: '653e80c0-48e1-4421-8533-c78580ad2d44/0',
+        text: 'Tropical Medicine Care Plan',
+      },
+    ],
+    metadata: {
+      dataSources: [],
+      requestedWhen: '',
+      isRedacted: false,
+    },
+    result: {
+      id: '123',
+      metadata: {
+        dataSources: [],
+        isRedacted: false,
+        requestedWhen: '',
+      },
+      status: {
+        system: 'https://leedsth.nhs.uk/ehr/request-group/status',
+        code: 'active',
+        display: 'Active',
+      },
+      intent: {
+        system: 'https://leedsth.nhs.uk/ehr/request-group/intent',
+        code: 'proposal',
+        display: 'Proposal',
+      },
+      action: [
+        {
+          elementId: '653e80c0-48e1-4421-8533-c78580ad2d44/0',
+          title: 'Launch Tropical Medicine Care Plan',
+          type: {
+            text: 'Create',
+            coding: [
+              {
+                system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-type',
+                code: 'create',
+                display: 'Create',
+              },
+            ],
+          },
+          extension: null,
+          code: [
+            {
+              coding: [
+                {
+                  system: 'https://leedsth.nhs.uk/ehr/guidance-action',
+                  code: 'action/care-plan/LTHT/c6b8b82a-f881-01fc-5d89-ad0290544577',
+                  display: 'Launch Care Plan',
+                },
+              ],
+            },
+          ],
+          selectionBehaviour: {
+            system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-selection-behaviour',
+            code: 'any',
+            display: 'Any',
+          },
+          requiredBehaviour: {
+            system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-required-behaviour',
+            code: 'could',
+            display: 'Could',
+          },
+        },
+      ],
+    },
+  },
+  {
+    id: 'c3f634e3-7c4c-4b0b-908b-ea96066f9f24',
+    status: GuidanceStatusCode.Success,
+    occuranceDateTime: {
+      kind: PartialDateTimeKindCode.DateTime,
+      value: '2025-11-24T14:15:59.6348992+00:00',
+    },
+    requestIdentifier: {
+      value: '65917780-dd50-4eda-8494-69d491af9e9e',
+    },
+    reasonCode: {
+      text: 'Test Guidance',
+    },
+    text: {
+      div: '',
+      text: 'Submit an eForm',
+    },
+    note: [
+      {
+        elementId: 'c3f634e3-7c4c-4b0b-908b-ea96066f9f24/0',
+        text: 'Skin Assessment eForm',
+      },
+    ],
+    metadata: {
+      dataSources: [],
+      requestedWhen: '',
+      isRedacted: false,
+    },
+    result: {
+      id: '123',
+      metadata: {
+        dataSources: [],
+        isRedacted: false,
+        requestedWhen: '',
+      },
+      status: {
+        system: 'https://leedsth.nhs.uk/ehr/request-group/status',
+        code: 'active',
+        display: 'Active',
+      },
+      intent: {
+        system: 'https://leedsth.nhs.uk/ehr/request-group/intent',
+        code: 'proposal',
+        display: 'Proposal',
+      },
+      action: [
+        {
+          elementId: 'c3f634e3-7c4c-4b0b-908b-ea96066f9f24/0',
+          title: 'Launch Skin Assessment form',
+          type: {
+            text: 'Create',
+            coding: [
+              {
+                system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-type',
+                code: 'create',
+                display: 'Create',
+              },
+            ],
+          },
+          extension: [
+            {
+              url: 'https://leedsth.nhs.uk/cds/template-name',
+              valueString: '4at-assessment-test-for-delirium-and-cognitive-impairment',
+            },
+          ],
+          code: [
+            {
+              coding: [
+                {
+                  system: 'https://leedsth.nhs.uk/ehr/guidance-action',
+                  code: 'action/form/4at-assessment-test-for-delirium-and-cognitive-impairment',
+                  display: '4AT Rapid Clinical Test For Delirium',
+                },
+              ],
+            },
+          ],
+          selectionBehaviour: {
+            system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-selection-behaviour',
+            code: 'any',
+            display: 'Any',
+          },
+          requiredBehaviour: {
+            system: 'https://leedsth.nhs.uk/ehr/request-group/action/action-required-behaviour',
+            code: 'could',
+            display: 'Could',
+          },
+        },
+      ],
+    },
+  },
+  guidanceOne,
+]
 
 // const guidanceList: GuidanceList = {
 //   id: '88686ed1-ac86-4e14-8c73-fc020f1b0e5b',
--- packages/storybook/src/clinical/organisms/guidances/guidances.test.tsx
+++ packages/storybook/src/clinical/organisms/guidances/guidances.test.tsx
@@ -1,10 +1,28 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import GuidanceSummary from '@ltht-react/guidance-summary'
-import guidances from './guidances.fixture'
+
+import guidances, { guidanceWithActions } from './guidances.fixture'
 
 describe('Guidances', () => {
   it('Renders', () => {
     render(<GuidanceSummary guidance={guidances[0]} />)
+  })
+
+  it('adds clickable action links', async () => {
+    const cb = jest.fn()
+
+    render(<GuidanceSummary guidance={guidanceWithActions[0]} onActionClick={cb} />)
+
+    await userEvent.click(screen.getByText('Launch Tropical Medicine Care Plan'))
+
+    expect(cb).toHaveBeenCalledTimes(1)
+  })
+
+  it("doesn't add clickable action links when click handler is excluded", () => {
+    render(<GuidanceSummary guidance={guidanceWithActions[0]} />)
+
+    expect(screen.queryByText('Launch Tropical Medicine Care Plan')).toBeNull()
   })
 })
