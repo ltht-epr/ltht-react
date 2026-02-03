f25e6ab521e89b58d35f3e57d02955ce80549da6
Jack Thorpe <jack.thorpe@airelogic.com>
2026-02-03 17:06:43 +0000

    Render guidance notes as anchor tags when they have an external link available

--- components/clinical/guidance-summary/src/atoms/guidance-note-cell.tsx
+++ components/clinical/guidance-summary/src/atoms/guidance-note-cell.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import Icon from '@ltht-react/icon'
 import { TABLE_COLOURS } from '@ltht-react/styles'
 import { Annotation } from '@ltht-react/types'
 import { FC } from 'react'
@@ -9,10 +10,32 @@ const NoteCell = styled.div`
   flex-grow: 1;
 `
 
+const ClickableNoteCell = styled.a`
+  padding: 0.5rem;
+  border: 1px solid ${TABLE_COLOURS.BORDER};
+  flex-grow: 1;
+  pointer: cursor;
+`
+
+const GuidanceNoteCell: FC<Props> = ({ note }) => {
+  const externalLink = note.extension?.find(
+    (x) => x?.url === 'https://leedsth.nhs.uk/ehr/guidance/additional-guidance-uri'
+  )?.valueString
+
+  const clickable = !!externalLink?.length
+
+  return clickable ? (
+    <ClickableNoteCell target="_blank" href={externalLink}>
+      {note.text}&nbsp;
+      <Icon type="external-link" size="medium" />
+    </ClickableNoteCell>
+  ) : (
+    <NoteCell>{note.text}</NoteCell>
+  )
+}
+
 interface Props {
   note: Annotation
 }
-
-const GuidanceNoteCell: FC<Props> = ({ note }) => <NoteCell>{note.text}</NoteCell>
 
 export default GuidanceNoteCell
